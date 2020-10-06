b82a46c0fa21291cf80929f3917027ad6447d5a8
taylorod <taylorsunwantedemails@gmail.com>
2020-10-06 11:51:05 -0500

    add it's own file for a very big sum

---
+++ javascript/a_very_big_sum.js
@@ -0,0 +1,15 @@
+// A Very Big Sum
+
+let numbers = [1000000001, 1000000002, 1000000003, 1000000004, 1000000005]
+
+function aVeryBigSum(ar) {
+
+  let result = 0
+  ar.forEach(function (number) {
+    result = result + number
+  })
+  return result
+
+}
+
+console.log(aVeryBigSum(numbers))
