0a35f52c60d3a14459d96c88e288f7ec34e13649
taylorod <taylorsunwantedemails@gmail.com>
2020-11-07 14:12:27 -0600

    update javascript todo file

--- todo_javascript.js
+++ todo_javascript.js
@@ -1,7 +1,5 @@
 // Insertion Sort - make folder, solve with and without swap, create explained file
 
-// Very Big Sum - make folder, create explained file
-
 // Two Number Sum - make folder, create explained file
 
 // Prime Number - solve without swap
