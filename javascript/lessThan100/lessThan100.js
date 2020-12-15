9e628a9b6b424ef9221345dd285730f3c16beddc
taylorod <taylorsunwantedemails@gmail.com>
2020-12-15 11:44:15 -0600

    add while loop solution

--- javascript/lessThan100/lessThan100.js
+++ javascript/lessThan100/lessThan100.js
@@ -31,3 +31,19 @@ const lessThan100forEach = function (array) {
 }
 
 console.log(lessThan100forEach([99, 101, 88, 4, 2000, 50]))
+
+// Solved using while loop
+const lessThan100While = function (array) {
+  let result = []
+  let index = 0
+
+  while (index < array.length) {
+    if (array[index] < 100) {
+      result.push(array[index])
+    }
+    index += 1
+  }
+  return result
+}
+
+console.log(lessThan100forEach([99, 101, 88, 4, 2000, 50]))
