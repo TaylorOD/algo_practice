c406f83b4e86a6e1855c7e0efb866413951b5e4b
taylorod <taylorsunwantedemails@gmail.com>
2020-12-17 11:14:42 -0600

    add add solution

---
+++ javascript/add/add.js
@@ -0,0 +1,10 @@
+// Write a function that returns the sum of two numbers.
+
+// For param1 = 1 and param2 = 2, the output should be
+// add(param1, param2) = 3.
+
+const add = function (param1, param2) {
+  return param1 + param2
+}
+
+console.log(add(1, 2))
---
+++ javascript/add/add_explained.js
@@ -0,0 +1,12 @@
+// Write a function that returns the sum of two numbers.
+
+// For param1 = 1 and param2 = 2, the output should be
+// add(param1, param2) = 3.
+
+// Create function called add that takes in two variables 
+const add = function (param1, param2) {
+  // Return variable 1 added with variable two
+  return param1 + param2
+}
+// print add with variables 1 and 2
+console.log(add(1, 2))
