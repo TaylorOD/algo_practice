72f62e6794f653d66ae972e45a98b6b090d3edf6
taylorod <taylorsunwantedemails@gmail.com>
2020-12-18 18:34:37 -0600

    add checks

--- javascript/reverse/reverse_explained.js
+++ javascript/reverse/reverse_explained.js
@@ -5,7 +5,12 @@
 // Create function that takes in a string
 function reverse (string) {
   // return the string after it has split the string, reversed the string, then rejoined the string.
-  return string.split("").reverse().join("")
+  if (!string || string.length < 2 || typeof !== "string") {
+    return false
+  } else {
+    return string.split("").reverse().join("")
+  }
+  
 }
 // Call the reverse function with input string
 console.log(reverse("About this course"))
