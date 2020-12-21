d1a124c2e021f58e73e5fc9655984f1e13ba1420
taylorod <taylorsunwantedemails@gmail.com>
2020-12-21 16:23:17 -0500

    add skip it solution and explaination

---
+++ javascript/skipIt/skipIt.js
@@ -0,0 +1,23 @@
+// Given an array of numbers, return a new array in which only select numbers from the original array are included, based on the following:
+
+// The new array should always start with the first number from the original array. 
+// The next number that should be included depends on what the first number is. 
+// The first number dictates how many spaces to the right the computer should move to pick the next number. 
+// For example, if the first number is 2, then the next number that the computer should select would be two spaces to the right. 
+// This number gets added to the new array. If this next number happens to be a 4, then the next number after that is the one four spaces to the right. 
+// And so on and so forth until the end of the array is reached.
+
+// Input: [2, 1, 3, 2, 5, 1, 2, 6, 2, 7, 1, 5, 6, 3, 2, 6, 2, 1, 2]
+
+// Output: [2, 3, 1, 2, 2, 1, 5, 2, 2]
+
+const skipIt = function (array) {
+  let result = []
+  for (let i = 0; i < array.length; i += array[i]) {
+    result.push(array[i])
+  }
+  return result
+
+}
+
+console.log(skipIt([2, 1, 3, 2, 5, 1, 2, 6, 2, 7, 1, 5, 6, 3, 2, 6, 2, 1, 2]))
