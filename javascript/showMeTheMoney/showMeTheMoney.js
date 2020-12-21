2d8d0fbfc8ec7c908951db5690d4af59101313d9
taylorod <taylorsunwantedemails@gmail.com>
2020-12-21 16:52:41 -0500

    add show me the money solution

---
+++ javascript/showMeTheMoney/showMeTheMoney.js
@@ -0,0 +1,32 @@
+// Given a string, return true if the “$” character is contained within the string or false if it is not.
+
+// Input: “i hate $ but i love money i know i know im crazy”
+// Output: true
+
+// Input: “abcdefghijklmnopqrstuvwxyz”
+// Output: false
+
+// Using .includes method 
+const showMeTheMoney = function (string) {
+  if (string.includes("$")) {
+    return true
+  } else {
+    return false
+  }
+}
+
+console.log(showMeTheMoney("abcdefghijklmnopqrstuvwxyz"))
+
+// Using a while loop
+const showMeTheMoneyLoop = function (string) {
+  let index = 0
+  while (index < string.length) {
+    if (string[index] === "$") {
+      return true
+    }
+    index += 1
+  }
+  return false
+}
+
+console.log(showMeTheMoneyLoop("abcdefghijklmnopqrstuvwxyz"))
---
+++ javascript/showMeTheMoney/showMeTheMoney_explained.js
@@ -0,0 +1,41 @@
+// Given a string, return true if the “$” character is contained within the string or false if it is not.
+
+// Input: “i hate $ but i love money i know i know im crazy”
+// Output: true
+
+// Input: “abcdefghijklmnopqrstuvwxyz”
+// Output: false
+
+// Using .includes method 
+// Create function that takes in a string
+const showMeTheMoney = function (string) {
+  // If the string includes the $ symbol then return true.
+  if (string.includes("$")) {
+    return true
+    // Else return false
+  } else {
+    return false
+  }
+}
+// Call the function with the input string which will return false
+console.log(showMeTheMoney("abcdefghijklmnopqrstuvwxyz"))
+
+// Using a while loop
+// Create function that takes in a string
+const showMeTheMoneyLoop = function (string) {
+  // Create an index that we can use to increment our way through our string
+  let index = 0
+  // Create a while loop that will loop through each element in the string
+  while (index < string.length) {
+    // If the current element in the string is equal to the $ symbol then return true
+    if (string[index] === "$") {
+      return true
+    }
+    // Increment the index each loop
+    index += 1
+  }
+  // Return false if none of the string elements are equal to $
+  return false
+}
+// Call the function with the input string which will return false
+console.log(showMeTheMoneyLoop("abcdefghijklmnopqrstuvwxyz"))
