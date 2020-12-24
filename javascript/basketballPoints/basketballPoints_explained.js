47476c9d181dbe5d0df1c55dab099021a6fc10da
taylorod <taylorsunwantedemails@gmail.com>
2020-12-23 20:24:38 -0500

    add explained file

--- javascript/basketballPoints/basketballPoints_explained.js
+++ javascript/basketballPoints/basketballPoints_explained.js
@@ -5,9 +5,10 @@
 // points(1, 1) ➞ 5
 // points(7, 5) ➞ 29
 
-
+// Create function that takes in two variables 
 function points(twoPointers, threePointers) {
+  // Return the two pointer variable multiplied by 2 + the three pointer variable multiplied by 3
 	return (twoPointers * 2) + (threePointers * 3)
 }
-
+// Call points function with 7 and 5
 console.log(points(7, 5))
