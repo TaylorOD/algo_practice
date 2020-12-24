3cc9c007cb92565b49f274926811cb7550e8ce5b
taylorod <taylorsunwantedemails@gmail.com>
2020-12-23 20:23:05 -0500

    add solution for basketball points

---
+++ javascript/basketballPoints/basketballPoints.js
@@ -0,0 +1,12 @@
+// You are counting points for a basketball game, given the amount of 2-pointers scored and 3-pointers scored, 
+// find the final points for the team and return that value.
+
+// Examples
+// points(1, 1) ➞ 5
+// points(7, 5) ➞ 29
+
+function points(twoPointers, threePointers) {
+	return (twoPointers * 2) + (threePointers * 3)
+}
+
+console.log(points(7, 5))
