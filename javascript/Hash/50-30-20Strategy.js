6d4543a40a2e410919ec433138b7a44cfa0df90c
taylorod <taylorsunwantedemails@gmail.com>
2020-12-27 12:31:08 -0500

    add solution for 503020

---
+++ javascript/Hash/50-30-20Strategy.js
@@ -0,0 +1,24 @@
+// The 50-30-20 strategy is a simple way to budget, which involves spending 50% of after-tax income on needs, 30% after tax income on wants, and 20% after-tax income on savings or paying off debt.
+
+// Given the after-tax income as ati, what you are supposed to do is to make a function that will return an object that shows how much a person needs to spend on needs, wants, and savings.
+
+// Examples
+// fiftyThirtyTwenty(10000) ➞ { "Needs": 5000, "Wants": 3000, "Savings": 2000 }
+
+// fiftyThirtyTwenty(50000) ➞ { "Needs": 25000, "Wants": 15000, "Savings": 10000 }
+
+// fiftyThirtyTwenty(13450) ➞ { "Needs": 6725, "Wants": 4035, "Savings": 2690 }
+
+
+function fiftyThirtyTwenty(ati) {
+	let result = {}
+	
+	result.Needs = ati * .50
+	result.Wants = ati * .30
+	result.Savings = ati * .20
+	
+	
+	return result
+}
+
+console.log(fiftyThirtyTwenty(10000))
