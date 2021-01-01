f1310815ad8289d094878e5f03411f8365f9f97e
taylorod <taylorsunwantedemails@gmail.com>
2021-01-01 14:22:27 -0500

    add solution for 3and5 and explained

--- javascript/Number/3and5/3and5.js
+++ javascript/Number/3and5/3and5.js
@@ -4,7 +4,15 @@
 // Find the sum of all the multiples of 3 or 5 below 1000.
 
 const threeAndFive = function (number) {
-
+  let sum = 0
+  for (let i = 0; i < number; i += 1) {
+    
+    if (i % 3 === 0 || i % 5 === 0) {
+      sum = sum + i
+    }
+  }
+  return sum
 }
 
+console.log(threeAndFive(10))
 console.log(threeAndFive(1000))
--- javascript/Number/3and5/3and5_exaplined.js
+++ javascript/Number/3and5/3and5_exaplined.js
@@ -2,3 +2,21 @@
 // The sum of these multiples is 23.
 
 // Find the sum of all the multiples of 3 or 5 below 1000.
+
+// Create function that takes in a number
+const threeAndFive = function (number) {
+  // Create a sum variable that we will add to as we loop through the array
+  let sum = 0
+  // Create a for loop that will run until it is at the input number
+  for (let i = 0; i < number; i += 1) {
+    // If the index element is divisible by 3 or by 5 than increase the sum by the index
+    if (i % 3 === 0 || i % 5 === 0) {
+      sum = sum + i
+    }
+  }
+  // Return the sum
+  return sum
+}
+// Call the function with the test input
+console.log(threeAndFive(10))
+console.log(threeAndFive(1000))
