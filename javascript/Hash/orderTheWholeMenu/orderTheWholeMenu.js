37fa7450120f7b35cf10f56d8510b14c4f9c15b3
taylorod <taylorsunwantedemails@gmail.com>
2021-01-03 19:28:47 -0600

    add order whole menu problem

---
+++ javascript/Hash/orderTheWholeMenu/orderTheWholeMenu.js
@@ -0,0 +1,15 @@
+// Given a hash, where the keys are strings representing food items, 
+// and the values are numbers representing the price of each food, 
+// return the amount someone would pay if they'd order one of each food from the entire menu.
+
+// Input: {"hot dog" = 2, "hamburger" => 3, "steak sandwich" => 5, "fries" => 1, "cole slaw" => 1, "soda" => 2}
+
+// Output: 14
+
+// Explanation: If someone would order one of everything on the menu, they'd pay a total of 14 (the sum of all the hash's values).
+
+const orderTheWholeMenu = function (menuHash) {
+  
+}
+
+console.log(orderTheWholeMenu({"hot dog" = 2, "hamburger" = 3, "steak sandwich" = 5, "fries" = 1, "cole slaw" = 1, "soda" = 2}))
