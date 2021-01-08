6cd6f1376b3b14f0058903e4b54c6ec3e049feec
taylorOD <taylorsunwantedemails@gmail.com>
2021-01-08 09:35:39 -0600

    add solution for DNA question

--- javascript/Hash/RNATranscription/RNATranscription.js
+++ javascript/Hash/RNATranscription/RNATranscription.js
@@ -13,3 +13,25 @@
 
 // Input: 'ACGTGGTCTTAA'
 // Output: 'UGCACCAGAAUU'
+
+const RNATranscription = function (RNAString) {
+  let result = ""
+  for (let i = 0; i < RNAString.length; i += 1) {
+    result = result + (DNAHelperFunction(RNAString[i]))
+  }
+  return result
+}
+
+const DNAHelperFunction = function (RNAStringElement) {
+  if (RNAStringElement === "A") {
+    return "U"
+  } else if (RNAStringElement === "T") {
+    return "A"
+  } else if (RNAStringElement === "C") {
+    return "G"
+  } else if (RNAStringElement === "G") {
+    return "C"
+  }
+}
+
+console.log(RNATranscription("ACGTGGTCTTAA"))
