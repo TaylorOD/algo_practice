99f3092f02d64187c9637e9743b0b486f840825c
taylorOD <taylorsunwantedemails@gmail.com>
2021-01-08 09:40:14 -0600

    add solution explination for RNA question

--- javascript/Hash/RNATranscription/RNATranscription_explained.js
+++ javascript/Hash/RNATranscription/RNATranscription_explained.js
@@ -14,24 +14,35 @@
 // Input: 'ACGTGGTCTTAA'
 // Output: 'UGCACCAGAAUU'
 
+// Create function that takes in the RNA input string
 const RNATranscription = function (RNAString) {
+  // Create an empty result string to store out results
   let result = ""
+  // Create a for loop that will loop over the length of our input string
   for (let i = 0; i < RNAString.length; i += 1) {
+    // Call a helper function (created to clean up this solution) to return the correct DNA character when fed a RNA character
+    // Add the return of the helper function and the existing elements in the result string to our result string
     result = result + (DNAHelperFunction(RNAString[i]))
   }
+  // Return the results string
   return result
 }
 
+// Create a helper function that takes in a RNA String Element
 const DNAHelperFunction = function (RNAStringElement) {
+  // If the RNAStringElement is "A" then return "U"
   if (RNAStringElement === "A") {
     return "U"
+  // If the RNAStringElement is "T" then return "A"
   } else if (RNAStringElement === "T") {
     return "A"
+  // If the RNAStringElement is "C" then return "G"
   } else if (RNAStringElement === "C") {
     return "G"
+  // If the RNAStringElement is "G" then return "C"
   } else if (RNAStringElement === "G") {
     return "C"
   }
 }
-
+// Call the RNA function with the givin test input
 console.log(RNATranscription("ACGTGGTCTTAA"))
