09067d9ba806c4203a0377536c3cec538db2afba
taylorod <taylorsunwantedemails@gmail.com>
2021-01-11 06:43:19 -0600

    add start of hash exercise

---
+++ javascript/Hash/anagrams/anagrams.js
@@ -0,0 +1,10 @@
+// Given two strings, return true if they are anagrams of each other, and false if they are not. 
+// An anagram is a word, phrase, or name formed by rearranging the letters of another, such as cinema, formed from iceman.
+
+// Do not use any built-in sort methods.
+
+// Input: “silent”, “listen”
+// Output: true
+
+// Input: “frog”, “bear”
+// Output: false
