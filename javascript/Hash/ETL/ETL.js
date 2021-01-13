1f12808c2a0e3f6616a8eafb1e40e5ee32b76b05
taylorod <taylorsunwantedemails@gmail.com>
2021-01-13 07:42:51 -0600

    add file set up for etl question

---
+++ javascript/Hash/ETL/ETL.js
@@ -0,0 +1,23 @@
+// You are given two parameters, an array and a number. 
+// Return a hash whose keys are each of the values from the array parameter, and whose values are the number parameter.
+
+// Input:
+
+// First argument: ["a", "e", "i", "o", "u"]
+// Second argument: 1
+
+// Output:
+
+// {
+// 'a' => 1,
+// 'e' => 1,
+// 'i' => 1,
+// 'o' => 1,
+// 'u' => 1
+// }
+
+const ETL = function (array, number) {
+
+}
+
+console.log(ETL(["a", "e", "i", "o", "u"], 1))
---
+++ javascript/Hash/ETL/ETL_explained.js
@@ -0,0 +1,17 @@
+// You are given two parameters, an array and a number. 
+// Return a hash whose keys are each of the values from the array parameter, and whose values are the number parameter.
+
+// Input:
+
+// First argument: ["a", "e", "i", "o", "u"]
+// Second argument: 1
+
+// Output:
+
+// {
+// 'a' => 1,
+// 'e' => 1,
+// 'i' => 1,
+// 'o' => 1,
+// 'u' => 1
+// }
