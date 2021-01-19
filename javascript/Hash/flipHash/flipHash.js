0c0157ef566dcb3484d24cec98c065eb5bfccf89
taylorod <taylorsunwantedemails@gmail.com>
2021-01-19 07:16:48 -0700

    add new question

---
+++ javascript/Hash/flipHash/flipHash.js
@@ -0,0 +1,14 @@
+// Given a hash, create a new hash that has the keys and values switched.
+
+// Input: {"a" => 1, "b" => 2, "c" => 3}
+// Output: {1 => "a", 2 => "b", 3 => "c"}
+
+const flipHash = function (inputHash) {
+  let result = {}
+  for (let key in inputHash) {
+    
+  }
+  return result
+}
+
+console.log(flipHash({"a": 1, "b": 2, "c": 3}))
