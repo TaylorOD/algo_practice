371523d830f700ca49c9c37d019c0a7464c9b888
taylorod <taylorsunwantedemails@gmail.com>
2021-01-19 20:53:41 -0700

    add second solution for ETL problem

--- javascript/Other/ETL2/ETL2.js
+++ javascript/Other/ETL2/ETL2.js
@@ -40,6 +40,19 @@ console.log(hashAToHashB({ 1: ["A", "E", "I", "O", "U"] }))
 // 'g': 2
 // }
 
+const hashAToHashBTwo = function (inputHash) {
+  let result = {}
+
+  for (let i = 0; i < inputHash[1].length; i += 1) {
+    result[inputHash[1][i].toLowerCase()] = 1
+    result[inputHash[2][i].toLowerCase()] = 2
+  }
+
+  return result
+}
+
+console.log(hashAToHashBTwo({ 1: ["A", "E"], 2: ["D", "G"] }))
+
 // Input:
 // 1: ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
 // 2: ["D", "G"],
