ea0764e571fa8e634a15e1bb02f897827fd99efc
taylorod <taylorsunwantedemails@gmail.com>
2021-01-20 07:45:01 -0700

    add complete data two problem start files and code

--- javascript/Other/ETL2/ETL2.js
+++ javascript/Other/ETL2/ETL2.js
@@ -53,14 +53,15 @@ const hashAToHashBTwo = function (inputHash) {
 
 console.log(hashAToHashBTwo({ 1: ["A", "E"], 2: ["D", "G"] }))
 
-// Input:
-// 1: ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
-// 2: ["D", "G"],
-// 3: ["B", "C", "M", "P"],
-// 4: ["F", "H", "V", "W", "Y"],
-// 5: ["K"],
-// 8: ["J", "X"],
-// 10: ["Q", "Z"]
+// Input: 
+// {
+//   1: ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
+//   2: ["D", "G"],
+//   3: ["B", "C", "M", "P"],
+//   4: ["F", "H", "V", "W", "Y"],
+//   5: ["K"],
+//   8: ["J", "X"],
+//   10: ["Q", "Z"]
 // }
 
 // Output:
@@ -92,3 +93,29 @@ console.log(hashAToHashBTwo({ 1: ["A", "E"], 2: ["D", "G"] }))
 // 'y': 4,
 // 'z': 10
 // }
+
+const hashAToHashBThree = function (inputHash) { // Not finished
+  let result = {}
+  for (let i = 0; i < inputHash; i += 1) {
+    result[inputHash[1][i].toLowerCase()] = 1
+    result[inputHash[3][i].toLowerCase()] = 3
+    result[inputHash[3][i].toLowerCase()] = 3
+    result[inputHash[2][i].toLowerCase()] = 2
+
+    // result[inputHash[5][i].toLowerCase()] = 5
+    // result[inputHash[8][i].toLowerCase()] = 8
+    // result[inputHash[10][i].toLowerCase()] = 10
+
+  }
+  return result
+}
+
+console.log(hashAToHashBThree({
+  1: ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
+  2: ["D", "G"],
+  3: ["B", "C", "M", "P"],
+  4: ["F", "H", "V", "W", "Y"],
+  5: ["K"],
+  8: ["J", "X"],
+  10: ["Q", "Z"]
+}))
---
+++ javascript/Other/completeTheDataTwo/completeTheDataTwo.js
@@ -0,0 +1,46 @@
+// Given an array of social media posts and an array of users, return a list of posts (as an array of hashes) that replaces the submitted_by id number as the person's actual name.
+
+// For example, given this array of posts (note that the submitted_by is an id number):
+
+// [
+// {title: 'Me Eating Pizza', submitted_by: 231, likes: 1549},
+// {title: 'i never knew how cool i was until now', submitted_by: 989, likes: 3},
+// {title: 'best selfie evar!!!', submitted_by: 111, likes: 1092},
+// {title: 'Mondays are the worst', submitted_by: 403, likes: 644}
+// ]
+
+// And this array of users:
+
+// [
+// {user_id: 403, name: "Aunty Em"},
+// {user_id: 231, name: "Joelle P."},
+// {user_id: 989, name: "Lyndon Johnson"},
+// {user_id: 111, name: "Patti Q."},
+// ]
+
+// Return the array of posts as follows:
+
+// [
+// {title: 'Me Eating Pizza', submitted_by: "Joelle P.", likes: 1549},
+// {title: 'i never knew how cool i was until now', submitted_by: "Lyndon Johnson", likes: 3},
+// {title: 'best selfie evar!!!', submitted_by: "Patti Q.", likes: 1092},
+// {title: 'Mondays are the worst', submitted_by: "Aunty Em", likes: 644}
+// ]
+
+const completeTheDataTwo = function (inputPostArray, inputUserArray) {
+
+}
+
+console.log(completeTheDataTwo([
+  {title: 'Me Eating Pizza', submitted_by: 231, likes: 1549},
+  {title: 'i never knew how cool i was until now', submitted_by: 989, likes: 3},
+  {title: 'best selfie evar!!!', submitted_by: 111, likes: 1092},
+  {title: 'Mondays are the worst', submitted_by: 403, likes: 644}
+  ],
+  [
+    {user_id: 403, name: "Aunty Em"},
+    {user_id: 231, name: "Joelle P."},
+    {user_id: 989, name: "Lyndon Johnson"},
+    {user_id: 111, name: "Patti Q."},
+  ]
+  ))
