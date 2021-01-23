db7b5aed318f741f2da366b8165d15e771ede891
taylorod <taylorsunwantedemails@gmail.com>
2021-01-23 10:03:21 -0700

    add solution for twoSum problem

--- javascript/Array/twoSum/twoSum.js
+++ javascript/Array/twoSum/twoSum.js
@@ -8,3 +8,20 @@
 
 // Input: [1, 2, 3, 4, 5]
 // Output: false (While 1, 2, 3, and 4 altogether add up to 10, we're seeking just one pair of numbers.)
+
+const twoSum = function (inputArray) {
+
+  for (let i = 0; i < inputArray.length; i += 1) {
+    for (let j = 1; j < inputArray.length; j += 1) {
+      if (inputArray[i] != inputArray[j]) {
+        if (inputArray[i] + inputArray[j] === 10) {
+          return [inputArray[i], inputArray[j]]
+        }
+      } 
+    }
+  }
+  return false
+}
+
+console.log(twoSum([2, 5, 3, 1, 0, 7, 11]))
+console.log(twoSum([1, 2, 3, 4, 5]))
--- javascript/Array/twoSum/twoSum_explained.js
+++ javascript/Array/twoSum/twoSum_explained.js
@@ -8,3 +8,20 @@
 
 // Input: [1, 2, 3, 4, 5]
 // Output: false (While 1, 2, 3, and 4 altogether add up to 10, we're seeking just one pair of numbers.)
+
+const twoSum = function (inputArray) {
+  let result = []
+
+  for (let i = 0; i < inputArray.length; i += 1) {
+    for (let j = 1; i < inputArray.length; j += 1) {
+      if (inputArray[i] + inputArray[j] === 10) {
+        result.push(inputArray[i])
+        result.push(inputArray[j])
+      } 
+    }
+  }
+  return result
+}
+
+console.log(twoSum([2, 5, 3, 1, 0, 7, 11]))
+console.log(twoSum([1, 2, 3, 4, 5]))
