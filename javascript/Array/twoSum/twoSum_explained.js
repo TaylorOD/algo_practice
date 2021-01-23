18ab515d37e3928b663485aff98ffb09ad59aa4c
taylorod <taylorsunwantedemails@gmail.com>
2021-01-23 10:07:22 -0700

    add explained.js for twoSum

--- javascript/Array/twoSum/twoSum_explained.js
+++ javascript/Array/twoSum/twoSum_explained.js
@@ -9,19 +9,24 @@
 // Input: [1, 2, 3, 4, 5]
 // Output: false (While 1, 2, 3, and 4 altogether add up to 10, we're seeking just one pair of numbers.)
 
+// Create function that takes in a inputArray
 const twoSum = function (inputArray) {
-  let result = []
-
+// Create out first for loop
   for (let i = 0; i < inputArray.length; i += 1) {
-    for (let j = 1; i < inputArray.length; j += 1) {
-      if (inputArray[i] + inputArray[j] === 10) {
-        result.push(inputArray[i])
-        result.push(inputArray[j])
+    // Create our second for loop that starts at index 1 rather than 0
+    for (let j = 1; j < inputArray.length; j += 1) {
+      // Create if condition that will keep us from returning two of the same numbers like [5, 5] as the result
+      if (inputArray[i] != inputArray[j]) {
+        // If the two selected elements being looped over equal ten than return those elements
+        if (inputArray[i] + inputArray[j] === 10) {
+          return [inputArray[i], inputArray[j]]
+        }
       } 
     }
   }
-  return result
+  // If there is no elements that equal 10 than return false.
+  return false
 }
-
+// Call function with both test inputs
 console.log(twoSum([2, 5, 3, 1, 0, 7, 11]))
 console.log(twoSum([1, 2, 3, 4, 5]))
