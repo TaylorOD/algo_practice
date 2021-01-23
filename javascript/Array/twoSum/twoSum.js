586c68b5c6593ce777143a2a202b60d8c1aec483
taylorod <taylorsunwantedemails@gmail.com>
2021-01-23 09:46:37 -0700

    add start of problem

---
+++ javascript/Array/twoSum/twoSum.js
@@ -0,0 +1,10 @@
+// Given an array of numbers, return a new array containing just two numbers (from the original array) that add up to the number 10. 
+// If there are no two numbers that add up to 10, return false.
+
+// Specifically use nested loops to solve this exercise even though there are other approaches as well.
+
+// Input: [2, 5, 3, 1, 0, 7, 11]
+// Output: [3, 7]
+
+// Input: [1, 2, 3, 4, 5]
+// Output: false (While 1, 2, 3, and 4 altogether add up to 10, we're seeking just one pair of numbers.)
