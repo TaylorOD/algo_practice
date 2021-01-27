faed68a2338d42d4887388eb1c929cb1a6644752
taylorod <taylorsunwantedemails@gmail.com>
2021-01-27 07:00:38 -0700

    add start file for question from data stuctures book

---
+++ javascript/Hash/intersectionOfTwoArrays/intersectionOfTwoArrays.js
@@ -0,0 +1,19 @@
+// Write a function that returns the intersection of two arrays.
+// The intersection is a third array that contains all values contained within the first two arrays.
+
+// Input:
+// [1, 2, 3, 4, 5], [0, 2, 4, 6, 8]
+
+// Output:
+// [2, 4]
+
+// Your function should have a time complexity of O(N).
+// Do not use any build in programing language methods to do this.
+// Use a hash table in your solution.
+
+
+const intersectionOfTwoArrays = function (inputArrayOne, inputArrayTwo) {
+
+}
+
+console.log(intersectionOfTwoArrays([1, 2, 3, 4, 5], [0, 2, 4, 6, 8]))
