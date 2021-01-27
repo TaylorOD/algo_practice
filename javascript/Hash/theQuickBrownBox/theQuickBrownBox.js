7eb4c8eaf7b02ebff58fb4ef1068d871f4934d9e
taylorod <taylorsunwantedemails@gmail.com>
2021-01-27 07:13:21 -0700

    add the quick brown box question from data structures book

---
+++ javascript/Hash/theQuickBrownBox/theQuickBrownBox.js
@@ -0,0 +1,15 @@
+// Write a function that accepts a string that contains all the letters of the alphabet except one and returns the missing letter.
+
+// Input:
+// "the quick brown box jumps over the lazy dog"
+
+// Output:
+// "f"
+
+// The function should have a time complexity of O(N)
+
+const theQuickBrownBox = function (inputString) {
+
+}
+
+console.log(theQuickBrownBox("the quick brown box jumps over the lazy dog"))
---
+++ javascript/Hash/theQuickBrownBox/theQuickBrownBox_Explained.js
@@ -0,0 +1,9 @@
+// Write a function that accepts a string that contains all the letters of the alphabet except one and returns the missing letter.
+
+// Input:
+// "the quick brown box jumps over the lazy dog"
+
+// Output:
+// "f"
+
+// The function should have a time complexity of O(N)
