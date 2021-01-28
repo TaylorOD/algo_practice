4b3284b6c761faf922f56b3aa82d0b65f529210c
taylorod <taylorsunwantedemails@gmail.com>
2021-01-28 07:05:36 -0700

    add starting files for arraySubset

---
+++ javascript/Hash/arraySubset/arraySubset_Explained.js
@@ -0,0 +1,9 @@
+// Given two arrays, determine whether one is a subset of the other. It is considered a subset if all the values in one array are contained within the other.
+
+// NOTE: You must accomplish this in O(n) time. This is also known as linear time.
+
+// Input: [1, 2, 3, 4, 5, 6], [6, 3, 2]
+// Output: true
+
+// Input: [1, 2, 3, 4, 5, 6], [6, 3, 7]
+// Output: false
