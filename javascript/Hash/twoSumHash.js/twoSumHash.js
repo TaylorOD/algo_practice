188cec99204f2d1e1ef342da1d80fb03433702cc
taylorod <taylorsunwantedemails@gmail.com>
2021-01-29 07:12:11 -0700

    add twoSum Hash setup

---
+++ javascript/Hash/twoSumHash.js/twoSumHash.js
@@ -0,0 +1,16 @@
+// This is the same exercise as Two Sum I, but you must now solve it in linear time.
+
+// Given an array of numbers, return a new array containing just two numbers (from the original array) that add up to the number 10. If there are no two numbers that add up to 10, return false.
+
+// Input: [2, 5, 3, 1, 0, 7, 11]
+// Output: [3, 7]
+
+// Input: [1, 2, 3, 4, 5]
+// Output: false (While 1, 2, 3, and 4 altogether add up to 10, we're seeking just one pair of numbers.)
+
+const twoSumHash = function (inputArray) {
+
+}
+
+console.log(twoSumHash([2, 5, 3, 1, 0, 7, 11]))
+console.log(twoSumHash([1, 2, 3, 4, 5]))
