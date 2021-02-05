d92a084e7795376084b58e9a4e88be968f2f40af
taylorod <taylorsunwantedemails@gmail.com>
2021-02-05 07:06:07 -0700

    add setup for recursion staircase problem

---
+++ javascript/Recursion/algoLadderStarter/staircaseProblem/staircaseProblem.js
@@ -0,0 +1,7 @@
+// We have a staircase of N steps, and a person has the ability to climb one, two, or three steps at a time.
+// How many different possible "paths" can someone take to reach the top?
+// Write a recursive function that will calculate this for N steps.
+
+const staircaseProblem = function (numberOfSteps) {}
+
+console.log(staircaseProblem(5))
