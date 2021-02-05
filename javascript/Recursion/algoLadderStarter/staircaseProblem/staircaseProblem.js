88f465cf0552b46d7959e364a7865677a8627bc0
taylorod <taylorsunwantedemails@gmail.com>
2021-02-05 07:19:25 -0700

    add solution for staircase problem

--- javascript/Recursion/algoLadderStarter/staircaseProblem/staircaseProblem.js
+++ javascript/Recursion/algoLadderStarter/staircaseProblem/staircaseProblem.js
@@ -2,6 +2,14 @@
 // How many different possible "paths" can someone take to reach the top?
 // Write a recursive function that will calculate this for N steps.
 
-const staircaseProblem = function (numberOfSteps) {}
+const staircaseProblem = function (numberOfSteps) {
+  if (numberOfSteps < 0) {
+    return 0
+  } else if (numberOfSteps === 1 || numberOfSteps === 0) {
+    return 1
+  } else {
+    return staircaseProblem(numberOfSteps - 1) + staircaseProblem(numberOfSteps - 2) + staircaseProblem(numberOfSteps - 3)
+  }
+}
 
-console.log(staircaseProblem(5))
+console.log(staircaseProblem(6))
--- javascript/Recursion/algoLadderStarter/staircaseProblem/staircaseProblem_explained.js
+++ javascript/Recursion/algoLadderStarter/staircaseProblem/staircaseProblem_explained.js
@@ -2,8 +2,20 @@
 // How many different possible "paths" can someone take to reach the top?
 // Write a recursive function that will calculate this for N steps.
 
+// Create function that takes in an inputNumberOfSteps
 const staircaseProblem = function (numberOfSteps) {
-
+  // If the number of steps left if less than zero than return zero
+  if (numberOfSteps < 0) {
+    return 0
+    // Else if the number of steps left is equal to 1 or 0 then return 1
+  } else if (numberOfSteps === 1 || numberOfSteps === 0) {
+    return 1
+    // Else run our function recursively adding the number of steps to our total for 1, 2, and 3 possible steps.
+  } else {
+    return (
+      staircaseProblem(numberOfSteps - 1) + staircaseProblem(numberOfSteps - 2) + staircaseProblem(numberOfSteps - 3)
+    )
+  }
 }
-
-console.log(staircaseProblem(5))
+// Call our function with a test input
+console.log(staircaseProblem(6))
