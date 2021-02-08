3eea406a858804ae9ff4026dfe25eb5368dc5d33
taylorod <taylorsunwantedemails@gmail.com>
2021-02-08 07:15:23 -0700

    add trigular number problem setup

---
+++ javascript/Recursion/algoLadderStarter/triangularNumbers/triangularNumbers_explained.js
@@ -0,0 +1,19 @@
+// There is a numerical sequence known as "Triangular Numbers."
+// The pattern begins as 1, 3, 6, 10, 15, 21, and continues onward with the Nth number in the pattern which is N plus the previous number.
+// For example the 7th number in the sequence is 28 since it's 7 (which is N) plus 21 (the previous number in the sequence).
+// Write a function that accepts a number for N and returns the correct number fro the series.
+// Example:
+// Input:
+// 7
+// Output:
+// 28
+
+// Input:
+// 9
+// Output:
+// 45
+
+const triangularNumbers = function (inputNumber) {}
+
+console.log(triangularNumbers(7))
+// console.log(triangularNumbers(9))
