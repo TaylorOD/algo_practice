b69d76599f2c80e6827bbeb45780ac2f7166b19d
taylorod <taylorsunwantedemails@gmail.com>
2021-02-09 06:49:26 -0700

    add recursion practice question

---
+++ javascript/Recursion/algoLadderStarter/xIndex/xIndex.js
@@ -0,0 +1,13 @@
+// Write a recursive function that accepts a string and returns the first index that contains the character "x".
+// You can assume there will be at least one "x"
+// Example:
+// Input:
+// "abcdefghijklmnopqrstuvwxyz"
+// Output:
+// 23
+
+const xIndex = function (inputString) {
+
+}
+
+console.log(xIndex("abcdefghijklmnopqrstuvwxyz"))
