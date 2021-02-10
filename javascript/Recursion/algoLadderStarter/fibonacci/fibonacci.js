99abd763aa8803dbd3cbd37f23b34bd9a9880055
taylorod <taylorsunwantedemails@gmail.com>
2021-02-10 07:22:13 -0700

    add setup for fib question

---
+++ javascript/Recursion/algoLadderStarter/fibonacci/fibonacci.js
@@ -0,0 +1,15 @@
+// The Fibonacci sequence is a mathematical sequence of numbers that goes like this until infinity:
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
+// Each subsequent number is the sum of the previous two numbers in the sequence.
+// Write a recursive function that will give you the Nth number when N is passed in.
+// Example:
+// Input:
+// 9
+// Output:
+// 55
+
+const fibonacci = function (number, memo = {}) {
+
+}
+
+console.log(fibonacci(9))
