c3e27541586f4b18bb1fec600d0f85fc3ba9996a
taylorod <taylorsunwantedemails@gmail.com>
2021-02-12 07:11:31 -0700

    add memo to algo

--- javascript/Recursion/algoLadderStarter/uniquePaths/uniquePaths.js
+++ javascript/Recursion/algoLadderStarter/uniquePaths/uniquePaths.js
@@ -14,3 +14,16 @@ const uniquePaths = function (rows, columns) {
 }
 
 console.log(uniquePaths(3, 7))
+
+// Using memoization to decrease the number of recursion calls we make
+const uniquePathsMemo = function (rows, columns, memo = {}) {
+  if (rows === 1 || columns === 1) {
+    return 1
+  }
+  if (!memo[[rows, columns]]) {
+    memo[[rows, columns]] = uniquePathsMemo(rows - 1, columns, memo) + uniquePathsMemo(rows, columns - 1, memo)
+  }
+  return memo[[rows, columns]]
+}
+
+console.log(uniquePathsMemo(3, 7))
