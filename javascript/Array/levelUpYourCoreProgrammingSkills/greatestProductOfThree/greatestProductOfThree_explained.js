6b8327fee6960a078daa515b68a38be7a23ef83e
taylorod <taylorsunwantedemails@gmail.com>
2021-02-17 07:06:49 -0700

    add question set up

---
+++ javascript/Array/levelUpYourCoreProgrammingSkills/greatestProductOfThree/greatestProductOfThree_explained.js
@@ -0,0 +1,8 @@
+// Given an array of positive numbers, write a function that returns the greatest product of any three numbers.
+// The approach of using three nested loops would clock in at O(N^3) which is very slow. Use sorting to implement the function in a way that it computes at O(N log N).
+
+const productOfThree = function (inputArray) {
+
+}
+
+console.log([1, 3, 2, 7, 9, 14, 12])
