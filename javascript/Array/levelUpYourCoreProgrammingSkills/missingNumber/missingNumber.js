5ee6d7cd22340bcd0219db89cae81ef54241ed34
taylorod <taylorsunwantedemails@gmail.com>
2021-02-17 07:40:23 -0700

    add question set up

---
+++ javascript/Array/levelUpYourCoreProgrammingSkills/missingNumber/missingNumber.js
@@ -0,0 +1,17 @@
+// Find the missing number from an array of integers.
+// The array is expected to have all the integers from 0 up to the arrays length but one is missing.
+// Write a function that takes only O(N log N)
+
+// Example:
+// Input: [5, 2, 4, 1, 0]
+// Output: 3
+
+// Input: [9, 3, 2, 5, 6, 7, 1, 0, 4]
+// Output: 8
+
+const missingNumber = function (inputArray) {
+
+}
+
+console.log(missingNumber([5, 2, 4, 1, 0]))
+console.log(missingNumber([9, 3, 2, 5, 6, 7, 1, 0, 4]))
