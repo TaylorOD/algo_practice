d8f427691e5d1f8ff4bdf5e566790599522e953a
taylorod <taylorsunwantedemails@gmail.com>
2021-02-22 07:47:15 -0700

    add algo solution for two array

--- javascript/Array/leetcodeEasy/twoArrays/twoArrays.js
+++ javascript/Array/leetcodeEasy/twoArrays/twoArrays.js
@@ -20,41 +20,62 @@
 // What if nums1's size is small compared to nums2's size? Which algorithm is better?
 // What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into the memory at once?
 
-const twoArrays = function (inputArrayOne, inputArrayTwo) {
-  let inputHash = {}
-  let results = []
+// const twoArrays = function (inputArrayOne, inputArrayTwo) {
+//   let inputHash = {}
+//   let results = []
 
-  for (var i = 0; i < inputArrayOne.length; i += 1) {
-    if (!inputHash[inputArrayOne[i]]) {
-      inputHash[inputArrayOne[i]] = 1
-    } else {
-      inputHash[inputArrayOne[i]] += 1
+//   for (var i = 0; i < inputArrayOne.length; i += 1) {
+//     if (!inputHash[inputArrayOne[i]]) {
+//       inputHash[inputArrayOne[i]] = 1
+//     } else {
+//       inputHash[inputArrayOne[i]] += 1
+//     }
+//   }
+
+//   for (let j = 0; j < inputArrayTwo.length; j += 1) {
+//     if (!inputHash[inputArrayTwo[j]]) {
+//       inputHash[inputArrayTwo[j]] = 1
+//     } else {
+//       inputHash[inputArrayTwo[j]] += 1
+//     }
+
+//     if (inputHash[inputArrayTwo[j]] === 2) {
+//       results.push(inputArrayTwo[j])
+//     }
+
+//     const indexInOne = inputArrayOne.indexOf(inputArrayOne[i])
+//     const indexInTwo = inputArrayTwo.indexOf(inputArrayTwo[j])
+
+//     if (inputHash[inputArrayOne[i]] >= 2 && indexInOne > -1) {
+//       results.push(inputArrayOne[i])
+//       inputHash[inputArrayOne[i]] = inputHash[inputArrayOne[i]] - 2
+//       inputArrayOne.splice(indexInOne, 1)
+//     }
+//   }
+
+//   return results
+// }
+
+// console.log(twoArrays((nums1 = [1, 2, 2, 1]), (nums2 = [2, 2])))
+// console.log(twoArrays((nums1 = [4, 9, 5]), (nums2 = [9, 4, 9, 8, 4])))
+
+
+const intersect = (nums1, nums2) => {
+  const results = []
+  const [short, long] = nums1.length <= nums2.length ? [nums1, nums2] : [nums2, nums1]
+
+  for (let i = 0; i < short.length; i++) {
+    const id = long.indexOf(short[i])
+    if (id > -1) {
+      results.push(short[i])
+      long.splice(id, 1)
     }
   }
 
-  for (let j = 0; j < inputArrayTwo.length; j += 1) {
-    if (!inputHash[inputArrayTwo[j]]) {
-      inputHash[inputArrayTwo[j]] = 1
-    } else {
-      inputHash[inputArrayTwo[j]] += 1
-    }
-
-    if (inputHash[inputArrayTwo[j]] === 2) {
-      results.push(inputArrayTwo[j])
-    }
-
-    const indexInOne = inputArrayOne.indexOf(inputArrayOne[i])
-    const indexInTwo = inputArrayTwo.indexOf(inputArrayTwo[j])
-
-    if (inputHash[inputArrayOne[i]] >= 2 && indexInOne > -1) {
-      results.push(inputArrayOne[i])
-      inputHash[inputArrayOne[i]] = inputHash[inputArrayOne[i]] - 2
-      inputArrayOne.splice(indexInOne, 1)
-    }
-  }
-
-  return results
+  return results.sort()
 }
 
-console.log(twoArrays((nums1 = [1, 2, 2, 1]), (nums2 = [2, 2])))
-console.log(twoArrays((nums1 = [4, 9, 5]), (nums2 = [9, 4, 9, 8, 4])))
+console.log(intersect([4, 9, 5], [9, 4, 9, 8, 4]))
+console.log(intersect([1, 2, 2, 1], [2, 2]))
+console.log(intersect([3, 1, 2], [1, 1]))
+console.log(intersect([4, 6, 1, 6, 6, 2], [1, 5, 5, 6, 1, 6, 1]))
--- javascript/Array/leetcodeEasy/twoArrays/twoArrays_explained.js
+++ javascript/Array/leetcodeEasy/twoArrays/twoArrays_explained.js
@@ -20,32 +20,61 @@
 // What if nums1's size is small compared to nums2's size? Which algorithm is better?
 // What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into the memory at once?
 
-const twoArrays = function (inputArrayOne, inputArrayTwo) {
-  let inputHash = {}
-  let results = []
+// const twoArrays = function (inputArrayOne, inputArrayTwo) {
+//   let inputHash = {}
+//   let results = []
 
-  for (let i = 0; i < inputArrayOne.length; i += 1) {
+//   for (let i = 0; i < inputArrayOne.length; i += 1) {
 
-    if (!inputHash[inputArrayOne[i]]) {
-      inputHash[inputArrayOne[i]] = 1
-    } else {
-      inputHash[inputArrayOne[i]] += 1
+//     if (!inputHash[inputArrayOne[i]]) {
+//       inputHash[inputArrayOne[i]] = 1
+//     } else {
+//       inputHash[inputArrayOne[i]] += 1
+//     }
+//   }
+
+//   for (let j = 0; j < inputArrayTwo.length; j += 1) {
+//     if (!inputHash[inputArrayTwo[j]]) {
+//       inputHash[inputArrayTwo[j]] = 1
+//     } else {
+//       inputHash[inputArrayTwo[j]] += 1
+//     }
+
+//     if (inputHash[inputArrayTwo[j]] === 2) {
+//       results.push(inputArrayTwo[j])
+//     }
+//   }
+//   return results
+// }
+
+// console.log(twoArrays((nums1 = [1, 2, 2, 1]), (nums2 = [2, 2])))
+// console.log(twoArrays((nums1 = [4, 9, 5]), (nums2 = [9, 4, 9, 8, 4])))
+
+
+// Create a function that takes in two inputarrays
+const intersect = (nums1, nums2) => {
+  // Create a results array to store our results
+  const results = []
+  // Determine which array is shorter/longer and give it the appropriate length name
+  const [short, long] = nums1.length <= nums2.length ? [nums1, nums2] : [nums2, nums1]
+
+  // Create a for loop that loops over the length of the short array
+  for (let i = 0; i < short.length; i++) {
+    // Create a variable of the index the same element in the long array
+    const id = long.indexOf(short[i])
+    // If the id is greater than -1 then it exists in the long array
+    if (id > -1) {
+      // Add it to the results array
+      results.push(short[i])
+      // Remove it from the long array
+      long.splice(id, 1)
     }
   }
-
-  for (let j = 0; j < inputArrayTwo.length; j += 1) {
-    if (!inputHash[inputArrayTwo[j]]) {
-      inputHash[inputArrayTwo[j]] = 1
-    } else {
-      inputHash[inputArrayTwo[j]] += 1
-    }
-
-    if (inputHash[inputArrayTwo[j]] === 2) {
-      results.push(inputArrayTwo[j])
-    }
-  }
-  return results
+  // REturn the results sorted
+  return results.sort()
 }
-
-console.log(twoArrays((nums1 = [1, 2, 2, 1]), (nums2 = [2, 2])))
-console.log(twoArrays((nums1 = [4, 9, 5]), (nums2 = [9, 4, 9, 8, 4])))
+// Call our function with several test inputs
+console.log(intersect([4, 9, 5], [9, 4, 9, 8, 4]))
+console.log(intersect([1, 2, 2, 1], [2, 2]))
+console.log(intersect([3, 1, 2], [1, 1]))
+console.log(intersect([4, 6, 1, 6, 6, 2], [1, 5, 5, 6, 1, 6, 1]))
