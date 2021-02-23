87fefacadff046067f49c298d3d04ddd287093cd
taylorod <taylorsunwantedemails@gmail.com>
2021-02-23 07:49:42 -0700

    finsih linked list implementation in ruby

--- ruby/data_stuctures/linked_list.rb
+++ ruby/data_stuctures/linked_list.rb
@@ -52,4 +52,56 @@ class LinkedList
     # If we get through the entire list without finding the data, we return nil:
     return nil
   end
+
+  def insert_at_index(index, value)
+    # We create the new nose with the provided value:
+    new_node = Node.new(value)
+
+    # If we are inserting at the beginning of the list:
+    if index == 0
+      # Have our new nose link to what was the first node:
+      new_node.next_node = first_node
+      # Establish that our new nose is now the list's first node:
+      self.first_node = new_node
+      return
+    end
+
+    # If we are inserting anywhere other than the beginning:
+    current_node = first_node
+    current_index = 0
+
+    # First, we access the node immediately before where the new nose will go:
+    while current_index < (index - 1) do
+      current_node = current_node.next_node
+      current_index += 1
+    end
+
+    # Have the new node link to the next node:
+    new_node.next_node = current_node.next_node
+
+    # Modify the link of the previous node to point to our new node:
+    current_node.next_node = new_node
+  end
+
+  def delete_at_index(index)
+    # If we are deleting the first node:
+    if index == 0
+      # Simply set the first node to be what is currently the second node:
+      self.first_node = first_node.next_node
+      return
+    end
+
+    current_node = first_node
+    current_index = 0
+
+    # First, we want to find the node immediately before the one we want to delete and call it current_node:
+    while current_index < (index, 1) do
+      current_node = current_node.next_node
+      current_index += 1
+    end
+    # We find the node that comes after the one we're deleting:
+    node_after_deleted_node = current_node.next_node.next_node
+
+    # We change the link of the current_node to point to the node_after_deleted_node, leaving the node we want to delete out of the list:
+    current_node.next_node = node_after_deleted_node
 end
