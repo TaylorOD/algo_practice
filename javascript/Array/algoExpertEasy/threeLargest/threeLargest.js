13218a04151184e296fe1d99140373ed80895440
taylorod <taylorsunwantedemails@gmail.com>
2021-03-01 08:33:32 -0600

    add three largeste solution

---
+++ javascript/Array/algoExpertEasy/threeLargest/threeLargest.js
@@ -0,0 +1,30 @@
+function findThreeLargestNumbers(inputArray) {
+  const threeLargest = [null, null, null]
+
+  for (const number of inputArray) {
+    updateThreeLargest(threeLargest, number)
+  }
+  return threeLargest
+}
+
+function updateThreeLargest(threeLargest, number) {
+  if (threeLargest[2] === null || number > threeLargest[2]) {
+    shiftAndUpdate(threeLargest, number, 2)
+  } else if (threeLargest[1] === null || number > threeLargest[1]) {
+    shiftAndUpdate(threeLargest, number, 1)
+  } else if (threeLargest[0] === null || number > threeLargest[0]) {
+    shiftAndUpdate(threeLargest, number, 0)
+  }
+}
+
+function shiftAndUpdate(threeLargestInputArray, number, index) {
+  for (let i = 0; i <= index; i += 1) {
+    if (i === index) {
+      threeLargestInputArray[i] = number
+    } else {
+      threeLargestInputArray[i] = threeLargestInputArray[i + 1]
+    }
+  }
+}
+
+console.log(findThreeLargestNumbers([141, 1, 17, -7, -17, -27, 18, 541, 8, 7, 7]))
