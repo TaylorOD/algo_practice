bb973c923ca845cd9a296d15c741cd7439fd9d8d
taylorod <taylorsunwantedemails@gmail.com>
2021-03-02 08:04:51 -0600

    add search to binary search tree in ruby

--- ruby/data_stuctures/binary_search_tree/binary_search_tree.rb
+++ ruby/data_stuctures/binary_search_tree/binary_search_tree.rb
@@ -4,4 +4,16 @@ def tree_node
     self.rightChild = right
     self.leftChild = left
   end
+
+  def search(searchValue, node)
+    # Basecase: if the node is nonexistent or if we've found the value we're looking for:
+    if node == nil || node.value == searchValue
+      return node
+    # If the value is less than the current node, perform the search on the left child:
+    elsif searchValue < node.value
+      return search(searchValue, node.leftChild)
+    # If the value is greater than the current node, perform the search on the right child:
+    else
+      return search(searchValue, node.rightChild)
+    end
 end
