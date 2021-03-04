37abfa96b112bbec55016dab0248c721614f2c67
taylorod <taylorsunwantedemails@gmail.com>
2021-03-04 07:39:06 -0600

    add deletion for binary tree

--- ruby/data_stuctures/binary_search_tree/binary_search_tree.rb
+++ ruby/data_stuctures/binary_search_tree/binary_search_tree.rb
@@ -35,4 +35,56 @@ def tree_node
       end
     end
   end
+
+  def delete(valueToDelete, node)
+    # The base case is when we've hit the bottom of the tree, and the parent node has no children
+    if node == nil
+      return nil
+     # If the value we're deleting is less or greater than the current node
+     # we set the left or right child respectively to be the return value of a
+     # recursive call of this method on the current node's left of right subtree
+    elsif valueToDelete < node.value
+      node.leftChild = delete(valueToDelete, node.leftChild)
+
+      # We return the current node (and its subtree if existent)
+      # to be used as the new value of its parent's left or right child:
+      return node
+
+     # If the current node is the one we want to delete:
+    elsif valueToDelete == node.value
+      # If the current node has no left child, we delete it by
+      # returning its right child (and its subtree if it exists)
+      # to be its parent's new subtree
+      if node.leftChild == nil
+        return node.rightChild
+
+        # (If the current node has no left OR right children, this ends up being Nil
+        # as per the first line of code in this function)
+      elsif node.rightChild == nil
+        return none.leftChild
+
+        # If the current node has two children, we delete the current node by calling the lift function (below),
+        # which changes the current node's value to the value of its successor node
+      else
+        node.rightChild = lift(node.rightChild, node)
+        return node
+      end
+    end
+  end
+
+  def lift(node, nodeToDelete)
+    # If the current node of this function has a left child,
+    # we recursively call this function to continue down the left subtree to find the successor node.
+    if node.leftChild
+      node.leftChild = lift(node.leftChild, nodeToDelete)
+      return node
+      # If the current node has no left child, that means the current node of this function
+      # is the successor node, and we take its value and make it the new value of the node
+      # that we're deleting
+    else
+      nodeToDelete.value = node.value
+      # We return the successor node's right child to be now used as its parent's left child
+      return node.rightChild
+    end
+  end
 end
