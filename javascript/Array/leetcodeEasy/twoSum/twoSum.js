659a3719d7845a1d4c3c45199eac5ce18a94872c
taylorod <taylorsunwantedemails@gmail.com>
2021-03-05 08:00:13 -0600

    add start and explained start of two sum

---
+++ javascript/Array/leetcodeEasy/twoSum/twoSum.js
@@ -0,0 +1,29 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+// Problem: leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/546/
+
+// Example 1:
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+// Example 2:
+
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+// Example 3:
+
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
+
+const twoSum = function (inputArray, target) {
+
+}
+
+console.log(twoSum([2, 7, 11, 15], 9))
+console.log(twoSum([3, 2, 4], 6))
+console.log(twoSum([3, 3] , 6))
