b228f23808bd5b87f3cd9c95d63166325a22a3a9
taylorod <taylorsunwantedemails@gmail.com>
2021-03-12 07:41:11 -0600

    add search to my trie

--- ruby/data_stuctures/tries/trie.rb
+++ ruby/data_stuctures/tries/trie.rb
@@ -8,4 +8,20 @@ class Trie
   def initialize(self)
     self.root = TrieNode()
   end
+
+  def search(self, word)
+    currentNode = self.root
+
+    for char in word
+      # If the current node has child key with current character:
+      if currentNode.children.get(char)
+        # Follow the child node:
+        currentNode = currentNode.children[char]
+      else
+        # If the current character isnt found amoung the current node's children,
+        # Our search word must not be in the trie:
+        return nil
+    end
+    return currentNode
+  end
 end
