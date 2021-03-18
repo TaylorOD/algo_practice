303493557cef7f4b75cb56e098753a1b164bfbe9
taylorod <taylorsunwantedemails@gmail.com>
2021-03-18 07:50:58 -0500

    built a dfs traverse for my graph

--- ruby/data_stuctures/graph/graph.rb
+++ ruby/data_stuctures/graph/graph.rb
@@ -9,6 +9,24 @@ class Vertex
   def add_adjacent_vertext(vertex)
     @adjacent_vertices << vertex
   end
+
+  def dfs_traverse(vertex, visited_vertices = {})
+    # Mark vertex as visited by adding it to the hash table:
+    visited_vertices[vertex.value] = true
+
+    # Print the vertex's value, so we can make sure our traversal really workds:
+    puts vertex.value
+
+    # Iterate through the current vertex's adjacent vertices:
+    vertex.adjacent_vertices.each do |adjacent_vertex|
+
+      # Ignore an adjacent vertex if we've already visited it:
+      next if visited_vertices[adjacent_vertex.value]
+
+      # Recursively call this method on the adjacent vertex:
+      dfs_traverse(adjacent_vertex, visited_vertices)
+    end
+  end
 end
 
 alice = Vertex.new("alice")
@@ -17,3 +35,5 @@ bob = Vertex.new("bob")
 alice.add_adjacent_vertext(bob)
 
 p alice
+
+alice.dfs_traverse(bob)
