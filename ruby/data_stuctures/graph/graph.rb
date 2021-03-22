37a1e7c46845129350920271009bea862e0b8771
taylorod <taylorsunwantedemails@gmail.com>
2021-03-22 07:10:50 -0500

    add breath first serach to graph

--- ruby/data_stuctures/graph/graph.rb
+++ ruby/data_stuctures/graph/graph.rb
@@ -79,6 +79,29 @@ class Vertex
       end
     end
   end
+
+  def breath_first_search_for_search(vertex, search_value, visited_vertices = {})
+    queue = Queue.new
+
+    visited_vertices[starting_vertex.value] = true
+    queue.enqueue(starting_vertex)
+
+    while queue.read
+      current_vertex = queue.dequeue
+
+      return current_vertex if current_vertex.value == search_value
+
+      current_vertex.adjacent_vertices.each do |adjacent_vertex|
+        if !visited_vertices[adjacent_vertex.value]
+          visited_vertices[adjacent_vertex.value] = true
+          queue.enqueue(adjacent_vertex)
+        end
+      end
+    end
+    # If we havn't found the vertex we're searching for
+    return nil
+  end
+
 end
 
 alice = Vertex.new("alice")
