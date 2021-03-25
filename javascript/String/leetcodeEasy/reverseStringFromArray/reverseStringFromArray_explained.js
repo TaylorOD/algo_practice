e2d0e36aa80ae13462c6f948dfa6b2e1a0e41d42
taylorod <taylorsunwantedemails@gmail.com>
2021-03-25 08:17:09 -0500

    add explained for reverseString

--- javascript/String/leetcodeEasy/reverseStringFromArray/reverseStringFromArray_explained.js
+++ javascript/String/leetcodeEasy/reverseStringFromArray/reverseStringFromArray_explained.js
@@ -9,12 +9,18 @@
 // Input: s = ["H","a","n","n","a","h"]
 // Output: ["h","a","n","n","a","H"]
 
+// Create a function that takes in an inputArray
 var reverseStringFromArray = function (inputArray) {
+  // Create a for loop where 0 and inputArray.length -1 are equated to first and second.
+  // While first is less that second the loop runs.
+  // First increments each loop and second decrements
   for (let [first, second] = [0, inputArray.length - 1]; first < second; first++, second--) {
-    ;[inputArray[first], inputArray[second]] = [inputArray[second], inputArray[first]]
+    // InputArray[first], and inputArray[second] are set to equal each other. Effectively swapping the values.
+    [inputArray[first], inputArray[second]] = [inputArray[second], inputArray[first]]
   }
+  // Return our modified array
   return inputArray
 }
-
+// Call out function with multiple test cases
 console.log(reverseStringFromArray(["h", "e", "l", "l", "o"]))
 console.log(reverseStringFromArray(["H", "a", "n", "n", "a", "h"]))
