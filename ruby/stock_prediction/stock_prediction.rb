cec5e0ee24f907c742b3e56db312112194e37caa
taylorod <taylorsunwantedemails@gmail.com>
2021-04-02 08:01:13 -0500

    add stock_prediction set up

---
+++ ruby/stock_prediction/stock_prediction.rb
@@ -0,0 +1,13 @@
+# You're working on some more stock prediction software.
+# The function you're writing accepts an array of preficted prices for a particular stock over the course of time.
+
+# Example:
+# input:
+# [10, 7, 5, 8, 11, 2, 6]
+# output:
+
+def stock_prediction(input_array)
+
+end
+
+p stock_prediction([10, 7, 5, 8, 11, 2, 6])
