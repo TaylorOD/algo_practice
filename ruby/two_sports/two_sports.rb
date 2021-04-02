0f1882b42bf7cba48015018e6019c0aa735f9503
taylorod <taylorsunwantedemails@gmail.com>
2021-04-02 08:26:51 -0500

    add next day attempt for two sports

--- ruby/two_sports/two_sports.rb
+++ ruby/two_sports/two_sports.rb
@@ -35,3 +35,39 @@ football_players = [
   { first_name: "Jill", last_name: "Huang", team: "Barleycorns" },
   { first_name: "Wanda", last_name: "Vakulskas", team: "Barleycorns" },
 ])
+
+
+# Next day attempt
+def two_sports_two(input_array_one, input_array_two)
+  athletes = {}
+  multi_athletes = []
+
+  input_array_one.each do |athlete|
+    athletes[athlete[:first_name] + athlete[:last_name]] = true
+  end
+
+  input_array_two.each do |athlete|
+    if athletes[athlete[:first_name] + athlete[:last_name]]
+      multi_athletes << athlete[:first_name] + " " + athlete[:last_name]
+    end
+  end
+
+
+  return multi_athletes
+end
+
+p two_sports_two(
+basketball_players = [
+  {first_name: "Jill", last_name: "Huang", team: "Gators"},
+  {first_name: "Janko", last_name: "Barton", team: "Sharks"},
+  {first_name: "Wanda", last_name: "Vakulskas", team: "Sharks"},
+  {first_name: "Jill", last_name: "Moloney", team: "Gators"},
+  {first_name: "Luuk", last_name: "Watkins", team: "Gators"}
+],
+football_players = [
+  { first_name: "Hanzla", last_name: "Radosti", team: "32ers" },
+  { first_name: "Tina", last_name: "Watkins", team: "Barleycorns" },
+  { first_name: "Alex", last_name: "Patel", team: "32ers" },
+  { first_name: "Jill", last_name: "Huang", team: "Barleycorns" },
+  { first_name: "Wanda", last_name: "Vakulskas", team: "Barleycorns" },
+])
