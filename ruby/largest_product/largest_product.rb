5dcf38c62d43d9fb4c21a5eb6ff1cf883b281f68
taylorod <taylorsunwantedemails@gmail.com>
2021-04-06 07:36:26 -0500

    add largest_product setup

---
+++ ruby/largest_product/largest_product.rb
@@ -0,0 +1,18 @@
+# Write a function that accepts an array of numbers and computes the highest product of any two numbers in the array.
+# At first glace this is easy as we can just find the two greatest nubers and mulitply them.
+# However, our array can contain negative numbers and look like this: [5, -10, -6, 9, 4]
+# In this case it's actually the product of the two lowerest numbers, -10 and -6 that would yield the highest product of 60.
+
+# Write a solution that is O(N)
+
+# Example:
+# Input:
+# [5, -10, -6, 9, 4]
+
+# Output:
+# 60
+
+def largest_product(input_array)
+end
+
+p largest_product([5, -10, -6, 9, 4])
