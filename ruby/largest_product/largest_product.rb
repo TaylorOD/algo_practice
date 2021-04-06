0c8b97c1dd97fe90e6fa7f154e909aaf70030740
taylorod <taylorsunwantedemails@gmail.com>
2021-04-06 07:54:11 -0500

    Add solution for largest product

--- ruby/largest_product/largest_product.rb
+++ ruby/largest_product/largest_product.rb
@@ -13,6 +13,35 @@
 # 60
 
 def largest_product(input_array)
+  greatest_number = -Float::INFINITY
+  second_greatest_number = -Float::INFINITY
+
+  lowest_number = Float::INFINITY
+  second_lowest_number = Float::INFINITY
+
+  input_array.each do |number|
+    if number >= greatest_number
+      second_greatest_number = greatest_number
+      greatest_number = number
+    elsif number > second_greatest_number
+      second_greatest_number = number
+    end
+
+    if number <= lowest_number
+      second_lowest_number = lowest_number
+      lowest_number = number
+    elsif number > lowest_number && number < second_lowest_number
+      second_lowest_number = number
+    end
+  end
+  greatest_product_high = greatest_number * second_greatest_number
+  greatest_product_low = lowest_number * second_lowest_number
+
+  if greatest_product_high > greatest_product_low
+    return greatest_product_high
+  else
+    return greatest_product_low
+  end
 end
 
 p largest_product([5, -10, -6, 9, 4])
