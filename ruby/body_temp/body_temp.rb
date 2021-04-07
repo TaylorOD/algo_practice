3ffeabbc6fe8dfc303eef9f253761f3895bee152
taylorod <taylorsunwantedemails@gmail.com>
2021-04-07 07:54:24 -0500

    finish body temp sort set up

---
+++ ruby/body_temp/body_temp.rb
@@ -0,0 +1,18 @@
+# We're creating software that analyez the date of body temperature readings taken from hundreds of human patients.
+# These readings are taken from healthy people and range from 97.0 degrees Fahrenheit to 99.0 degrees Fahrenheit.
+# An important point: Within this application, the decimal point never goes beyond the tenth place.
+# Write an algoithm that sorts these readings from lowest to heighest.
+# Write it faster than O(N log N) - Which is possible because there is a limited quantity of possible readings.
+
+# Example:
+# Input:
+# [98.6, 98.0, 97.1, 99.0, 98.9, 97.8, 98.5, 98.2, 98.0, 97.1]
+
+# Output:
+# [97.1, 97.1, 97.8, 98.0, 98.0, 98.2, 98.5, 98.6, 98.9, 99.0]
+
+def body_temp_sort(input_array)
+
+end
+
+p body_temp_sort([98.6, 98.0, 97.1, 99.0, 98.9, 97.8, 98.5, 98.2, 98.0, 97.1])
