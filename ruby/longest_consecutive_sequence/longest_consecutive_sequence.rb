55f9f4e4bdcb530477e24d2cb97e4650277152a6
taylorod <taylorsunwantedemails@gmail.com>
2021-04-08 08:05:07 -0500

    add longest sequence set up

---
+++ ruby/longest_consecutive_sequence/longest_consecutive_sequence.rb
@@ -0,0 +1,21 @@
+# Youre writing a function that accepts an array of unsorted integers and returns the length of the longest consecutive sequence amoung them.
+#  The sequence is formed by integers that increase by 1. For example:
+
+# Example:
+# Input:
+# [10, 5, 12, 3, 55, 30, 4, 11, 2]
+
+# [19,13, 15, 12, 18, 14, 17, 11]
+# Output:
+# [2, 3, 4, 5] is the sequence. 4 is the return value.
+
+# [11, 12, 13, 14, 15] is the sequence. 5 is the return value.
+
+# Do this in O(N) time.
+
+def longest_consecutive_sequence (input_array)
+
+end
+
+p longest_consecutive_sequence([10, 5, 12, 3, 55, 30, 4, 11, 2])
+p longest_consecutive_sequence([19,13, 15, 12, 18, 14, 17, 11])
