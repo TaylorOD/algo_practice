3ea937c5b346885859cbe58693da8d879b4dc4c7
taylorod <taylorsunwantedemails@gmail.com>
2021-04-08 07:53:54 -0500

    add solution

--- ruby/body_temp/body_temp.rb
+++ ruby/body_temp/body_temp.rb
@@ -12,7 +12,30 @@
 # [97.1, 97.1, 97.8, 98.0, 98.0, 98.2, 98.5, 98.6, 98.9, 99.0]
 
 def body_temp_sort(input_array)
+  hash_table = {}
 
+  input_array.each do |temperature|
+    if hash_table[temperature]
+      hash_table[temperature] += 1
+    elsif !hash_table[temperature]
+      hash_table[temperature] = 1
+    end
+  end
+
+  sorted_array = []
+
+  temperature = 970
+
+  while temperature <= 990
+    if hash_table[temperature / 10.0]
+      hash_table[temperature / 10.0].times do
+        sorted_array << temperature / 10.0
+      end
+    end
+    temperature += 1
+  end
+
+  return sorted_array
 end
 
 p body_temp_sort([98.6, 98.0, 97.1, 99.0, 98.9, 97.8, 98.5, 98.2, 98.0, 97.1])
