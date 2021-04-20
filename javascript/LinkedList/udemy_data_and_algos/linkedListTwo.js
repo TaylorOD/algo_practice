9fc949265d912b39e5d1beed618b6ce5f3c961c4
taylorod <taylorsunwantedemails@gmail.com>
2021-04-20 08:02:27 -0500

    learning how to use linkedLists

---
+++ javascript/LinkedList/udemy_data_and_algos/linkedListTwo.js
@@ -0,0 +1,12 @@
+class LinkedList {
+ constructor(value) {
+  this.head = {
+    value: value,
+    next: null,
+  }
+  this.tail = this.head
+  this.length = 1
+ }
+}
+let myLinkedList = new LinkedList(10)
+console.log(myLinkedList)
