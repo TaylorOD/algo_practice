690aa3834f0172d4d9d4c52ee8e4149463a905b9
taylorod <taylorsunwantedemails@gmail.com>
2021-05-11 08:16:53 -0500

    add fizzbuzz solution

---
+++ ruby/algo_ladder/fizzbuzz/fizzbuzz.rb
@@ -0,0 +1,23 @@
+# Write a function that prints out every number from 1 to N, with the following exceptions:
+
+# If the number is divisible by 3, print out "FIZZ".
+# If the number is divisible by 5, print out "BUZZ".
+# If the number is divisible by both 3 and 5, print out "FIZZBUZZ".
+
+def fizzbuzz (input_number)
+  index = 1
+  while index <= input_number
+    if index % 3 == 0 && index % 5 == 0
+      p "FIZZBUZZ"
+    elsif index % 3 == 0
+      p "FIZZ"
+    elsif index % 5 == 0
+      p "BUZZ"
+    else
+      p index
+    end
+    index += 1
+  end
+end
+
+fizzbuzz(25)
