5c047dfad1e3774a7e7f4d2c0afd825dc72fdb29
taylorod <taylorsunwantedemails@gmail.com>
2021-05-15 09:10:54 -0500

    Add prime solution in ruby

---
+++ ruby/algo_ladder/primes/primes_explained.rb
@@ -0,0 +1,26 @@
+# Write a function that returns whether a given number is a prime number.
+
+
+def prime (number)
+  if is_prime(number) == true
+    return true
+  else
+    return false
+  end
+end
+
+def is_prime (possible_prime)
+  half = possible_prime / 2
+  j = 2
+
+  while j <= half
+    if possible_prime % j == 0
+      return false
+    end
+    j += 1
+  end
+  return true
+end
+
+p prime(500)
+p prime(7)
