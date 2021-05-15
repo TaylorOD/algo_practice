37a491e5880dcd8e54e4b23bbb6e98f0c98bcd70
taylorod <taylorsunwantedemails@gmail.com>
2021-05-15 09:15:10 -0500

    add prime solution explained

--- ruby/algo_ladder/primes/primes_explained.rb
+++ ruby/algo_ladder/primes/primes_explained.rb
@@ -1,26 +1,35 @@
 # Write a function that returns whether a given number is a prime number.
 
-
+# Create a function that takes in a number
 def prime (number)
+  # Call helper function. If helper function return true then number is prime and return true
   if is_prime(number) == true
     return true
+    # Else number is not prime and return false
   else
     return false
   end
 end
 
+# Create helper function that takes in a possible prime number
 def is_prime (possible_prime)
+  # We only need to check number that are half of prime or less because that is how division works
   half = possible_prime / 2
-  j = 2
-
-  while j <= half
-    if possible_prime % j == 0
+  # Create an index
+  index = 2
+  # Create a while loop that runs until half
+  while index <= half
+    # If possible prime is ever divisable by index cleanly then the number is not prime and return false
+    if possible_prime % index == 0
       return false
     end
-    j += 1
+    # Increment the index
+    index += 1
   end
+  # If it is never false then its prime and return true
   return true
 end
 
+# Call our function with two test values
 p prime(500)
 p prime(7)
