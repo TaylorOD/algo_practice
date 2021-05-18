5da51360241a062293de747b4f1c6a4f8f9476c1
taylorod <taylorsunwantedemails@gmail.com>
2021-05-18 07:42:42 -0500

    add fibonacci_numbers setup

---
+++ ruby/algo_ladder/ fibonacci_numbers/ fibonacci_numbers.rb
@@ -0,0 +1,6 @@
+# Write a function that gives the Nth number of the Fibonacci Sequence. The Fibonacci sequence begins with 0 and 1, and every number thereafter is the sum of the previous two numbers. So the sequence goes like this:
+
+# 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, and so on until infinity...
+
+# Input: 9
+# Output: 21 (as this is the 9th number of the Fibonacci Sequence)
