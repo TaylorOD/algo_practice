79d1511cae48b6efbd03dc6dd1e83f138778418a
taylorod <taylorsunwantedemails@gmail.com>
2021-05-26 07:56:15 -0500

    add array mesh start

---
+++ ruby/algo_ladder/array_mesh_1/array_mesh_1.rb
@@ -0,0 +1,9 @@
+# Given two arrays of strings, return a new string that contains every combination of a string from the first array concatenated with a string from the second array.
+
+# Input: ["a", "b", "c"], ["d", "e", "f", "g"]
+# Output: ["ad", "ae", "af", "ag", "bd", "be", "bf", "bg", "cd", "ce", "cf", "cg"]
+
+def array_mesh_1 (input_array_one, input_array_two)
+end
+
+p array_mesh_1(["a", "b", "c"], ["d", "e", "f", "g"])
---
+++ ruby/algo_ladder/array_mesh_1/array_mesh_1_explained.rb
@@ -0,0 +1,4 @@
+# Given two arrays of strings, return a new string that contains every combination of a string from the first array concatenated with a string from the second array.
+
+# Input: ["a", "b", "c"], ["d", "e", "f", "g"]
+# Output: ["ad", "ae", "af", "ag", "bd", "be", "bf", "bg", "cd", "ce", "cf", "cg"]
