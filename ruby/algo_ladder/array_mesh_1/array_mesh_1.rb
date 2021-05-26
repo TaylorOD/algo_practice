71c49c5699fdeefd75d6d8f8578ed9d0b083a5d8
taylorod <taylorsunwantedemails@gmail.com>
2021-05-26 08:03:24 -0500

    add array_mesh_explained

--- ruby/algo_ladder/array_mesh_1/array_mesh_1.rb
+++ ruby/algo_ladder/array_mesh_1/array_mesh_1.rb
@@ -4,6 +4,17 @@
 # Output: ["ad", "ae", "af", "ag", "bd", "be", "bf", "bg", "cd", "ce", "cf", "cg"]
 
 def array_mesh_1 (input_array_one, input_array_two)
+  result = []
+  index_one = 0
+  while index_one < input_array_one.length
+    index_two = 0
+    while index_two < input_array_two.length
+      result << input_array_one[index_one] + input_array_two[index_two]
+      index_two += 1
+    end
+    index_one += 1
+  end
+  return result
 end
 
 p array_mesh_1(["a", "b", "c"], ["d", "e", "f", "g"])
--- ruby/algo_ladder/array_mesh_1/array_mesh_1_explained.rb
+++ ruby/algo_ladder/array_mesh_1/array_mesh_1_explained.rb
@@ -2,3 +2,28 @@
 
 # Input: ["a", "b", "c"], ["d", "e", "f", "g"]
 # Output: ["ad", "ae", "af", "ag", "bd", "be", "bf", "bg", "cd", "ce", "cf", "cg"]
+
+# Create a function that takes in two input arrays
+def array_mesh_1 (input_array_one, input_array_two)
+  # Create our results empty array
+  result = []
+  # Create an index
+  index_one = 0
+  # Create a while loop that will loop over out first array using our index_one
+  while index_one < input_array_one.length
+    # Create our second index
+    index_two = 0
+    # Create a second while loop that will loop over the second input array
+    while index_two < input_array_two.length
+      # Add the first character of the first array plus the first character of the second array to the results variable
+      # Increment over the second array to add each element with each character in the first array
+      result << input_array_one[index_one] + input_array_two[index_two]
+      index_two += 1
+    end
+    index_one += 1
+  end
+  # Return the result
+  return result
+end
+# Call test variable
+p array_mesh_1(["a", "b", "c"], ["d", "e", "f", "g"])
