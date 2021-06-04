a182e17354124a900825347a30a880c12a2cff04
taylorod <taylorsunwantedemails@gmail.com>
2021-06-04 08:52:49 -0500

    add solution and explained

--- ruby/algo_ladder/merge_sorted_arrays/merge_sorted_arrays.rb
+++ ruby/algo_ladder/merge_sorted_arrays/merge_sorted_arrays.rb
@@ -7,7 +7,31 @@
 # Modified A : [1, 5, 6, 8, 9]
 
 def merge_sorted_arrays (input_array_one, input_array_two)
+  index_one = 0
+  index_two = 0
+  result = []
 
+  if input_array_one.length > input_array_two.length
+    longer_array = input_array_one
+    shorter_array = input_array_two
+  else
+    longer_array = input_array_two
+    shorter_array = input_array_one
+  end
+
+  while index_one < longer_array.length || index_two < shorter_array.length
+    if longer_array[index_one] == nil
+      result << shorter_array[index_two]
+      index_two += 1
+    elsif longer_array[index_one] < shorter_array[index_two]
+      result << longer_array[index_one]
+      index_one += 1
+    else
+      result << shorter_array[index_two]
+      index_two += 1
+    end
+  end
+  return result
 end
 
 p merge_sorted_arrays([1, 5, 8], [6, 9])
--- ruby/algo_ladder/merge_sorted_arrays/merge_sorted_arrays_explained.rb
+++ ruby/algo_ladder/merge_sorted_arrays/merge_sorted_arrays_explained.rb
@@ -6,8 +6,39 @@
 
 # Modified A : [1, 5, 6, 8, 9]
 
+# Create a funciton that takes in two arrays
 def merge_sorted_arrays (input_array_one, input_array_two)
+  # Create two indexes and a result variable
+  index_one = 0
+  index_two = 0
+  result = []
 
+  # Check which array is longer
+  if input_array_one.length > input_array_two.length
+    longer_array = input_array_one
+    shorter_array = input_array_two
+  else
+    longer_array = input_array_two
+    shorter_array = input_array_one
+  end
+  # Run while loop while either index is less than their array length
+  while index_one < longer_array.length || index_two < shorter_array.length
+    # Make sure that we store the final numbers correctly after the first array is finished
+    if longer_array[index_one] == nil
+      result << shorter_array[index_two]
+      index_two += 1
+      # If the longer array element is less than add it to the results array and increment it
+    elsif longer_array[index_one] < shorter_array[index_two]
+      result << longer_array[index_one]
+      index_one += 1
+    # If the shorter array element is less than add it to the results array and increment it
+    else
+      result << shorter_array[index_two]
+      index_two += 1
+    end
+  end
+  # Return the results array
+  return result
 end
-
+# Call our function with our test array
 p merge_sorted_arrays([1, 5, 8], [6, 9])
