b719c8067de0be049a4d017a8d81fbd1a1b501f1
taylorod <taylorsunwantedemails@gmail.com>
2021-06-04 08:17:09 -0500

    set up files for stacks and queues

---
+++ ruby/algo_ladder/merge_sorted_arrays/merge_sorted_arrays.rb
@@ -0,0 +1,13 @@
+# Given two sorted arrays, merge the second array into the first array while ensuring that the first array remains sorted. Do not use any built-in sort methods.
+
+# Input :
+# A : [1, 5, 8]
+# B : [6, 9]
+
+# Modified A : [1, 5, 6, 8, 9]
+
+def merge_sorted_arrays (input_array_one, input_array_two)
+
+end
+
+p merge_sorted_arrays([1, 5, 8], [6, 9])
