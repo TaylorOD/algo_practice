933896e505ad0f609d64e70e313e80c661578632
taylorod <taylorsunwantedemails@gmail.com>
2021-06-09 07:27:31 -0600

    add working longest common solution

--- ruby/algo_ladder/longest_common_prefix/longest_common_prefix.rb
+++ ruby/algo_ladder/longest_common_prefix/longest_common_prefix.rb
@@ -14,3 +14,21 @@
 # Note:
 
 # All given inputs are in lowercase letters a-z.
+
+def longest_common_prefix(input_array_one)
+  result = ""
+  index_one = 0
+
+  while index_one < input_array_one.length
+    if input_array_one[0][index_one] == input_array_one[1][index_one] && input_array_one[0][index_one] == input_array_one[2][index_one]
+      result += input_array_one[0][index_one]
+      index_one += 1
+    else
+      return result
+    end
+  end
+  return result
+end
+
+p longest_common_prefix(["flower","flow","flight"])
+p longest_common_prefix(["dog","racecar","car"])
