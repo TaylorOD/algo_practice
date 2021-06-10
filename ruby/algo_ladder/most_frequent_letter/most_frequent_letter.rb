a086f2f3a498b3a578b6b8877ccaadc92e56ad5d
taylorod <taylorsunwantedemails@gmail.com>
2021-06-10 07:57:36 -0600

    add most frequent letter explained and solution in ruby

--- ruby/algo_ladder/most_frequent_letter/most_frequent_letter.rb
+++ ruby/algo_ladder/most_frequent_letter/most_frequent_letter.rb
@@ -4,5 +4,25 @@
 # Output: “p”
 
 def most_frequent_letter(input_string)
+  most_frequent_letter = ""
+  most_frequent_letter_count = 0
+  no_spaced_string = input_string.delete(" ")
+  letter_hash = {}
+  index_one = 0
+  while index_one < no_spaced_string.length
+    if !letter_hash[no_spaced_string[index_one]]
+      letter_hash[no_spaced_string[index_one]] = 1
+    else
+      letter_hash[no_spaced_string[index_one]] += 1
+    end
 
+    if letter_hash[no_spaced_string[index_one]] > most_frequent_letter_count
+      most_frequent_letter = no_spaced_string[index_one]
+      most_frequent_letter_count = letter_hash[no_spaced_string[index_one]]
+    end
+    index_one += 1
+  end
+  return most_frequent_letter
 end
+
+p most_frequent_letter("peter piper picked a peck of pickled peppers")
