fb036947f888ec9ed6d7b6d64c262787ddea8941
taylorod <taylorsunwantedemails@gmail.com>
2021-06-11 06:30:04 -0600

    add count_Votes start

---
+++ ruby/algo_ladder/count_votes/count_votes.rb
@@ -0,0 +1,12 @@
+# Given an array of strings, return a hash that provides of a count of how many times each string occurs.
+
+# Input: ["Dewey", "Truman", "Dewey", "Dewey", "Truman", "Truman", "Dewey", "Truman", "Truman", "Dewey", "Dewey"]
+
+# Output: {"Dewey" => 6, "Truman" => 5}
+
+# Explanation: "Dewey" occurs 6 times in the array, while "Truman" occurs 5 times.
+
+def count_votes(input_array)
+end
+
+p count_votes(["Dewey", "Truman", "Dewey", "Dewey", "Truman", "Truman", "Dewey", "Truman", "Truman", "Dewey", "Dewey"])
