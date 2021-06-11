e7ebd804e714da6feb53288f5606e9c95374c622
taylorod <taylorsunwantedemails@gmail.com>
2021-06-11 07:05:35 -0600

    add remove function for binary search tree

--- javascript/udemy_data_and_algos/binarySearchTree/binarySearchTree.js
+++ javascript/udemy_data_and_algos/binarySearchTree/binarySearchTree.js
@@ -51,13 +51,73 @@ class BinarySearchTree {
     }
     return false
   }
+  remove(value) {
+    if (!this.root) {
+      return false
+    }
+    let currentNode = this.root
+    let parentNode = null
+    while (currentNode) {
+      if (value < currentNode.value) {
+        parentNode = currentNode
+        currentNode = currentNode.left
+      } else if (value > currentNode.value) {
+        parentNode = currentNode
+        currentNode = currentNode.right
+      } else if (currentNode.value === value) {
+        if (currentNode.right === null) {
+          if (parentNode === null) {
+            this.root = currentNode.left
+          } else {
+            if (currentNode.value < parentNode.value) {
+              parentNode.left = currentNode.left
+            } else if (currentNode.value > parentNode.value) {
+              parentNode.right = currentNode.left
+            }
+          }
+        } else if (currentNode.right.left === null) {
+          currentNode.right.left = currentNode.left
+          if (parentNode === null) {
+            this.root = currentNode.right
+          } else {
+            if (currentNode.value < parentNode.value) {
+              parentNode.left = currentNode.right
+            } else if (currentNode.value > parentNode.value) {
+              parentNode.right = currentNode.right
+            }
+          }
+        } else {
+          let leftmost = currentNode.right.left
+          let leftmostParent = currentNode.right
+          while (leftmost.left !== null) {
+            leftmostParent = leftmost
+            leftmost = leftmost.left
+          }
+          leftmostParent.left = leftmost.right
+          leftmost.left = currentNode.left
+          leftmost.right = currentNode.right
+          if (parentNode === null) {
+            this.root = leftmost
+          } else {
+            if (currentNode.value < parentNode.value) {
+              parentNode.left = leftmost
+            } else if (currentNode.value > parentNode.value) {
+              parentNode.right = leftmost
+            }
+          }
+        }
+        return true
+      }
+    }
+  }
 }
 
+
 const tree = new BinarySearchTree()
+tree.insert(20)
 tree.insert(9)
 tree.insert(4)
 tree.insert(6)
-tree.insert(20)
 tree.insert(170)
 tree.insert(15)
 tree.insert(1)
