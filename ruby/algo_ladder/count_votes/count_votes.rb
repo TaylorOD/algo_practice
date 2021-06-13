7cdf43bbfb8d00fa8493de9628d5e02cea46bc58
taylorod <taylorsunwantedemails@gmail.com>
2021-06-13 15:53:37 -0600

    add count votes and count votes explained in ruby

--- ruby/algo_ladder/count_votes/count_votes.rb
+++ ruby/algo_ladder/count_votes/count_votes.rb
@@ -7,6 +7,17 @@
 # Explanation: "Dewey" occurs 6 times in the array, while "Truman" occurs 5 times.
 
 def count_votes(input_array)
+  result = {}
+  index_one = 0
+  while index_one < input_array.length
+    if (!result[input_array[index_one]])
+      result[input_array[index_one]] = 1
+    else
+      result[input_array[index_one]] += 1
+    end
+    index_one += 1
+  end
+  return result
 end
 
 p count_votes(["Dewey", "Truman", "Dewey", "Dewey", "Truman", "Truman", "Dewey", "Truman", "Truman", "Dewey", "Dewey"])
--- ruby/algo_ladder/count_votes/count_votes_explained.rb
+++ ruby/algo_ladder/count_votes/count_votes_explained.rb
@@ -6,7 +6,26 @@
 
 # Explanation: "Dewey" occurs 6 times in the array, while "Truman" occurs 5 times.
 
+# Create a function that takes in an array
 def count_votes(input_array)
+  # Create an empty results hash that we will use to store our results
+  result = {}
+  # Create and index
+  index_one = 0
+  # Create a while loop to loop through the length of our array
+  while index_one < input_array.length
+    # If the result doesnt exist in the hash then add it
+    if (!result[input_array[index_one]])
+      result[input_array[index_one]] = 1
+      # If it does exist then increment it
+    else
+      result[input_array[index_one]] += 1
+    end
+    # Increment our index by 1
+    index_one += 1
+  end
+  # Return our results array
+  return result
 end
-
+# Call our function with test inputs
 p count_votes(["Dewey", "Truman", "Dewey", "Dewey", "Truman", "Truman", "Dewey", "Truman", "Truman", "Dewey", "Dewey"])
