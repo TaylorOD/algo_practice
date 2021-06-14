a849696856273a4cfa8d8c5f87871027e96ced9b
taylorod <taylorsunwantedemails@gmail.com>
2021-06-14 07:08:24 -0600

    add start of graph

---
+++ javascript/udemy_data_and_algos/graphs/graph.js
@@ -0,0 +1,27 @@
+class Graph {
+  constructor() {
+    this.numberOfNodes = 0
+    this.adjacentList = {}
+  }
+  addVertex(node) {
+
+  }
+  addEdge(node1, node2) {
+
+  }
+  showConnections() {
+    const allNodes = Object.keys(this.adjacentList)
+    for (let node of allNodes) {
+      let nodeConnections = this.adjacentList[node]
+      let connections = ""
+      let vertex
+      for (vertex of nodeConnections) {
+        connections += vertex + " "
+      }
+      console.log(node + "-->" + connections)
+    }
+  }
+}
+const myGraph = new Graph()
+
+console.log(showConnections())
