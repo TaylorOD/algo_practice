03b8b3080dab6ee9f05bdddc184b5161ccaa0db7
taylorod <taylorsunwantedemails@gmail.com>
2021-06-15 21:01:56 -0600

    add rna_transcription set up

---
+++ ruby/algo_ladder/rna_transcription/rna_transcription_explained.rb
@@ -0,0 +1,15 @@
+# Given a DNA strand, return its RNA complement (per RNA transcription).
+
+# Both DNA and RNA strands are a sequence of nucleotides. Here we're representing the sequences with single-letter characters (e.g. a sample strand may look like: "AGCA".)
+
+# Given a string representing a DNA strand, provide its transcribed RNA strand, according to the following pattern:
+
+# G becomes C
+# C becomes G
+# T becomes A
+# A becomes U
+
+# So based on all this, here's a sample input/output:
+
+# Input: 'ACGTGGTCTTAA'
+# Output: 'UGCACCAGAAUU'
