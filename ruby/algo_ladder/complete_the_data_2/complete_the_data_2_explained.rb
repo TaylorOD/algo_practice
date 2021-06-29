a5ff18e31227ba4c3408332624fbf7426dfecf64
taylorod <taylorsunwantedemails@gmail.com>
2021-06-29 07:21:03 -0600

    add complete_the_Data two solution and explained

--- ruby/algo_ladder/complete_the_data_2/complete_the_data_2_explained.rb
+++ ruby/algo_ladder/complete_the_data_2/complete_the_data_2_explained.rb
@@ -27,17 +27,23 @@
 # {title: 'Mondays are the worst', submitted_by: "Aunty Em", likes: 644}
 # ]
 
+# Create function that takes in both of our data arrays
 def complete_the_data_2 (input_post_array, input_user_array)
+  # Create an each loop to move through out posts array
   input_post_array.each do |post|
+    # Create an each loop to move through our user array
     input_user_array.each do |user|
+      # If our post array submitted by is the same as our user user id
       if post[:submitted_by] == user[:user_id]
+        # Then update the submitted by to the user name
         post[:submitted_by] = user[:name]
       end
     end
   end
+  # Return the post array
   return input_post_array
 end
-
+# Name our two data arrays
 post_array = [
 {title: 'Me Eating Pizza', submitted_by: 231, likes: 1549},
 {title: 'i never knew how cool i was until now', submitted_by: 989, likes: 3},
@@ -51,5 +57,5 @@ user_array = [
 {user_id: 989, name: "Lyndon Johnson"},
 {user_id: 111, name: "Patti Q."},
 ]
-
+# Call our function and pass in our two data arrays
 p complete_the_data_2(post_array, user_array)
