23ea01bcb5b6b74fc5ef7f154b56a49232f1b30f
taylorod <taylorsunwantedemails@gmail.com>
2021-07-11 10:30:07 -0600

    update fibo memo

---
+++ javascript/udemy_data_and_algos/dynamicProgramming/fibonacciMemo.js
@@ -0,0 +1,46 @@
+const fibonacci = function (input_number, memo = {}) {
+  if (input_number > 2) {
+    return input_number
+  }
+  return fibonacci(input_number - 1, memo[input_number]) + fibonacci(input_number - 2, memo[input_number])
+}
+
+console.log(fibonacci(5))
+
+let calculations = 0
+function fibonacci(n) {
+  //O(2^n)
+
+  if (n < 2) {
+    return n
+  }
+  return fibonacci(n - 1) + fibonacci(n - 2)
+}
+
+function fibonacciMaster() {
+  //O(n)
+  let cache = {}
+  return function fib(n) {
+    calculations++
+    if (n in cache) {
+      return cache[n]
+    } else {
+      if (n < 2) {
+        return n
+      } else {
+        cache[n] = fib(n - 1) + fib(n - 2)
+        return cache[n]
+      }
+    }
+  }
+}
+
+function fibonacciMaster2(n) {
+  let answer = [0, 1]
+  for (let i = 2; i <= n; i++) {
+    answer.push(answer[i - 2] + answer[i - 1])
+  }
+  return answer.pop()
+}
+
+const fasterFib = fibonacciMaster()
