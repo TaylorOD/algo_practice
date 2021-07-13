15d0caa48ff5f55127570550054ee471ec931188
taylorod <taylorsunwantedemails@gmail.com>
2021-07-13 08:01:54 -0600

    add question link

--- javascript/leetcode/easy/powerOfThree/powerOfThree.js
+++ javascript/leetcode/easy/powerOfThree/powerOfThree.js
@@ -1,3 +1,5 @@
+// Question: https://leetcode.com/explore/interview/card/top-interview-questions-easy/102/math/745/
+
 // Given an integer n, return true if it is a power of three. Otherwise, return false.
 
 // An integer n is a power of three, if there exists an integer x such that n == 3x.
