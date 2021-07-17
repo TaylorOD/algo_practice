f890a29570002294fb7d5da6f141785ccdd0efc2
taylorod <taylorsunwantedemails@gmail.com>
2021-07-17 11:00:44 -0600

    Add max depth recursion and iterative solutions

--- javascript/leetcode/easy/maximumDepthOfBinaryTree/maximumDepthOfBinaryTree.js
+++ javascript/leetcode/easy/maximumDepthOfBinaryTree/maximumDepthOfBinaryTree.js
@@ -22,11 +22,71 @@
 // Input: root = [0]
 // Output: 1
 
-const maximumDepthOfBinaryTree = function (input_root) {
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
 
+// Create a function that takes in the LL/root node
+  // If check to see if ll/node.length grater than 1 is not then return 0
+  // Create counter that will be updated for each level of the node = start at 1
+  // Create left counter that will be updated for each level of the node = start at 1
+  // Create a while loop to traverse tree
+    // If the left node is not null decend and increment left counter
+    // If the right node is not null decend and increment right counter
+    // else
+      // Return left or right counter whichever is greater
+    //
+  //
+//
+
+const maximumDepthOfBinaryTree = function (input_root) {
+  if (!input_root) {
+    return 0
+  }
+  let level = 0
+  let queue = []
+  queue.push(input_root)
+
+  while (queue.length > 0) {
+    let count = queue.length
+    for (let index = 0; index < count; index += 1) {
+      const node = queue.shift()
+      if (node.right) {
+        queue.push(node.right)
+      }
+      if (node.left) {
+        queue.push(node.left)
+      }
+    }
+    level += 1
+  }
+  return level
 }
+
+const maximumDepthOfBinaryTreeRecursion = function (input_root) {
+  if (!input_root) {
+    return null
+  }
+  let max = Math.max(maxDepth(root.left), maxDepth(root.right))
+  return max + 1
+}
+
 
 maximumDepthOfBinaryTree([3, 9, 20, null, null, 15, 7])
 maximumDepthOfBinaryTree([1, null, 2])
 maximumDepthOfBinaryTree([])
 maximumDepthOfBinaryTree([0])
+
+maximumDepthOfBinaryTreeRecursion([3, 9, 20, null, null, 15, 7])
+maximumDepthOfBinaryTreeRecursion([1, null, 2])
+maximumDepthOfBinaryTreeRecursion([])
+maximumDepthOfBinaryTreeRecursion([0])
--- javascript/leetcode/easy/maximumDepthOfBinaryTree/maximumDepthOfBinaryTreeExplained.js
+++ javascript/leetcode/easy/maximumDepthOfBinaryTree/maximumDepthOfBinaryTreeExplained.js
@@ -22,9 +22,87 @@
 // Input: root = [0]
 // Output: 1
 
-const maximumDepthOfBinaryTree = function (input_root) {}
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
 
+// Create a function that takes in the LL/root node
+  // If check to see if ll/node.length grater than 1 is not then return 0
+  // Create counter that will be updated for each level of the node = start at 1
+  // Create left counter that will be updated for each level of the node = start at 1
+    // Create a while loop to traverse tree
+      // If the left node is not null decend and increment left counter
+      // If the right node is not null decend and increment right counter
+      // else
+      // Return left or right counter whichever is greater
+    //
+  //
+//
+
+// Create a function that takes in an input_root
+const maximumDepthOfBinaryTree = function (input_root) {
+  // If the input root tree is empty then return 0
+  if (!input_root) {
+    return 0
+  }
+  // Otherwise let the level counter start at 0
+  let level = 0
+  // Create a queue
+  let queue = []
+  // Add our root to the queue
+  queue.push(input_root)
+
+  // Create a while loop that will run as long as the queue length is greater than 0
+  while (queue.length > 0) {
+    // Create a count variable that is the queue length
+    let count = queue.length
+    // Create a for loop that will move through the queue
+    for (let index = 0; index < count; index += 1) {
+      // Create a node variable that is the queue shifted
+      const node = queue.shift()
+      // If node.right exists then push node.right to the queue
+      if (node.right) {
+        queue.push(node.right)
+      }
+      // If node.left exists then push node.left to the queue
+      if (node.left) {
+        queue.push(node.left)
+      }
+    }
+    // Increment our level function for each depth
+    level += 1
+  }
+  // Return the level variable
+  return level
+}
+// Create our recursive function that takes in an ll
+const maximumDepthOfBinaryTreeRecursion = function (input_root) {
+  // If the ll root is empty return null
+  if (!input_root) {
+    return null
+  }
+  // Create a variable that will loop at the max left and right decedents recursively
+  let max = Math.max(maximumDepthOfBinaryTreeRecursion(root.left), maximumDepthOfBinaryTreeRecursion(root.right))
+  // Return the max variable plus 1 for the initial depth
+  return max + 1
+}
+
+// Call both our functions with test inputs
 maximumDepthOfBinaryTree([3, 9, 20, null, null, 15, 7])
 maximumDepthOfBinaryTree([1, null, 2])
 maximumDepthOfBinaryTree([])
 maximumDepthOfBinaryTree([0])
+
+maximumDepthOfBinaryTreeRecursion([3, 9, 20, null, null, 15, 7])
+maximumDepthOfBinaryTreeRecursion([1, null, 2])
+maximumDepthOfBinaryTreeRecursion([])
+maximumDepthOfBinaryTreeRecursion([0])
