7b6379559709094d960f62056d13dfc246160178
taylorod <taylorsunwantedemails@gmail.com>
2021-07-22 07:46:35 -0600

    add explained for implementstrstr

--- javascript/leetcode/easy/implementstrStr/implementstrStrExplained.js
+++ javascript/leetcode/easy/implementstrStr/implementstrStrExplained.js
@@ -24,24 +24,30 @@
 // Output: 0
 
 // Create function that takes in needle and haystack
-// if needle is empty return 0
-// for loop
-// if haystack[index] === needle[0] - Need to address length of needle/full needle here
-// return index
-//
-//
+  // if needle is empty return 0
+  // for loop
+    // if haystack[index] === needle[0] - Need to address length of needle/full needle here
+      // return index
+    //
+  //
 // return -1
 //
 
+// Create a function that takes in haystack and needle
 const implementstrStr = function (haystack, needle) {
+  // If needle is an empty string return 0 per requirements
   if (needle === "") {
     return 0
   }
+  // Create a for loop that will move through the haystack string
   for (let index = 0; index < haystack.length; index += 1) {
+    // If the needle string is included in the haystack string then return the index it starts at
+    // Substring will look starting at the index passed in and for the length passed in as the second parameter
     if (haystack.substr(index, needle.length) === needle) {
       return index
     }
   }
+  // Return -1 if no match is found
   return -1
 }
 
