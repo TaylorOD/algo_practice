1c33d580e04b76745ef6cad9d92464f2325aa38a
taylorod <taylorsunwantedemails@gmail.com>
2021-07-24 10:32:09 -0600

    remove comments

--- javascript/leetcode/easy/climbingStairs/climbingStairs.js
+++ javascript/leetcode/easy/climbingStairs/climbingStairs.js
@@ -26,10 +26,8 @@ const climbingStairs = function (numberOfStairs) {
   let current = 1
 
   for (let index = 1; index <= numberOfStairs; index += 1) {
-
     current += previous
     previous = current - previous
-
   }
   return current
 }
@@ -37,22 +35,3 @@ const climbingStairs = function (numberOfStairs) {
 console.log(climbingStairs(2))
 console.log(climbingStairs(3))
 console.log(climbingStairs(13))
-
-// var climbStairs = function (n) {
-//   // First two pointers store the first two numbers of the Fibonacci sequence
-//   let prev = 0
-//   let curr = 1
-
-//   // We use a for loop to iterate from 1 up to our number n with our constraints  being: 1 <= n <= 45
-//   for (let i = 1; i <= n; i++) {
-//     // We store one side in our third pointer
-//     tmp = prev
-//     // We then update that side to be equal to the other pointer
-//     // This is because the next number is equal to the sum of the previous two numbers.
-//     prev = curr
-//     // Next we add tmp which now holds our lower number to curr which holds our upper number to get our next number.
-//     curr += tmp
-//   }
-//   // Outside of our loop we return curr which stored our cumulative total while we iterated.
-//   return curr
-// }
