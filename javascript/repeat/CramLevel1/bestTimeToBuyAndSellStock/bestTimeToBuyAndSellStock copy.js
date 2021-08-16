a759924cc0d2ec8e7d8c3ed1d2e434cdba09cd27
taylorod <taylorsunwantedemails@gmail.com>
2021-08-16 09:06:39 -0400

    add besttimetobuyandsell copy answer

---
+++ javascript/repeat/CramLevel1/bestTimeToBuyAndSellStock/bestTimeToBuyAndSellStock copy.js
@@ -0,0 +1,41 @@
+// You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+// You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+// Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+// Example 1:
+
+// Input: prices = [7,1,5,3,6,4]
+// Output: 5
+// Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+// Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+// Example 2:
+
+// Input: prices = [7,6,4,3,1]
+// Output: 0
+// Explanation: In this case, no transactions are done and the max profit = 0.
+
+// Create profit value
+// create min value
+// create for loop
+// update min
+  // else if update profit if current -min is greater
+// return profit
+
+const bestTimeToBuyAndSellStock = function (inputArray) {
+  let profit = 0
+  let min = inputArray[0]
+  for (let index = 0; index < inputArray.length; index += 1) {
+    if (min > inputArray[index]) {
+      min = inputArray[index]
+    } else if (profit < inputArray[index] - min) {
+      profit = inputArray[index] - min
+    }
+  }
+  return profit
+}
+
+console.log(bestTimeToBuyAndSellStock([7, 1, 5, 3, 6, 4]))
+console.log(bestTimeToBuyAndSellStock([7, 6, 4, 3, 1]))
