9d066a5d47a21776d5fe38dcdec2a4fda1843724
taylorod <taylorsunwantedemails@gmail.com>
2021-10-16 15:04:36 -0400

    add solution for missing number

---
+++ javascript/interviewing.io/mentorTwo/missingNumber/missingNumberExplained.js
@@ -0,0 +1,56 @@
+// https://leetcode.com/problems/missing-number/
+
+// Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+// Example 1:
+
+// Input: nums = [3,0,1]
+// Output: 2
+// Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+
+// Example 2:
+
+// Input: nums = [0,1]
+// Output: 2
+// Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
+
+// Example 3:
+
+// Input: nums = [9,6,4,2,3,5,7,0,1]
+// Output: 8
+// Explanation: n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.
+
+// Example 4:
+
+// Input: nums = [0]
+// Output: 1
+// Explanation: n = 1 since there is 1 number, so all numbers are in the range [0,1]. 1 is the missing number in the range since it does not appear in nums.
+
+// Create function that takes in input array
+// Create hash to store each number in
+// Loop through each number and add each number to hash
+// Loop and see if number in array exists in hash. If not return if
+//
+
+// Create a function that takes in the inputArray
+const missingNumber = (inputArray) => {
+  // Create a hash to store our numbers in
+  let numberHash = {}
+  // Create a for loop to move through each element of the input array and add it to our hash
+  for (let index = 0; index < inputArray.length; index += 1) {
+    numberHash[inputArray[index]] = true
+  }
+  // Create a for loop to move through our input array
+  for (let index = 0; index < inputArray.length; index += 1) {
+    // If the number does not exist in the hash return it
+    if (!numberHash[index]) {
+      return index
+    }
+  }
+  return inputArray.length
+}
+
+console.log(missingNumber([3, 0, 1]))
+console.log(missingNumber([0, 1]))
+console.log(missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1]))
+console.log(missingNumber([0]))
