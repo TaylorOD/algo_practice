c02d0c2726d3f532b3734dc3d1d3c1a8122ef72c
taylorod <taylorsunwantedemails@gmail.com>
2021-10-18 10:15:56 -0400

    add stupid sum query range solution

---
+++ javascript/interviewing.io/mentorTwo/rangeSumQuery/rangeSumQueryExplained.js
@@ -0,0 +1,48 @@
+// https://leetcode.com/problems/range-sum-query-immutable/
+
+// Given an integer array nums, handle multiple queries of the following type:
+
+// Calculate the sum of the elements of nums between indices left and right inclusive where left <= right.
+// Implement the NumArray class:
+
+// NumArray(int[] nums) Initializes the object with the integer array nums.
+// int sumRange(int left, int right) Returns the sum of the elements of nums between indices left and right inclusive (i.e. nums[left] + nums[left + 1] + ... + nums[right]).
+
+// Example 1:
+
+// Input
+// ["NumArray", "sumRange", "sumRange", "sumRange"]
+// [[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+// Output
+// [null, 1, -1, -3]
+
+// Explanation
+// NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
+// numArray.sumRange(0, 2); // return (-2) + 0 + 3 = 1
+// numArray.sumRange(2, 5); // return 3 + (-5) + 2 + (-1) = -1
+// numArray.sumRange(0, 5); // return (-2) + 0 + 3 + (-5) + 2 + (-1) = -3
+
+// Constraints:
+
+// 1 <= nums.length <= 104
+// -105 <= nums[i] <= 105
+// 0 <= left <= right < nums.length
+// At most 104 calls will be made to sumRange.
+
+const rangeSumQuery = (inputArray) => {
+  let map = [0]
+  let sum = 0
+  for (let index = 1; index < inputArray.length; index += 1) {
+    sum += nums[index - 1]
+    map[index] = sum
+  }
+  this.map = sum
+}
+
+NumArray.prototype.sumRange = function (left, right) {
+  let map = this.map
+  return map[right + 1] - map[left]
+}
+
+console.log(rangeSumQuery(["NumArray", "sumRange", "sumRange", "sumRange"]
+[[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]))
