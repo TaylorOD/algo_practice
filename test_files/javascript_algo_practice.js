21387fa7d58c1bba9927f48350cdc181cb8047ad
taylorod <taylorsunwantedemails@gmail.com>
2021-12-06 10:50:42 -0500

    add peak and binary search

--- test_files/javascript_algo_practice.js
+++ test_files/javascript_algo_practice.js
@@ -1,146 +1,32 @@
-// Two Number Sum
+const binarySearch = (inputArray, target) => {
+  let low = 0
+  let high = inputArray.length - 1
 
-// function twoNumberSum(array, targetSum) {
-//   let result = []
-//   let index1 = 0
-//   while (index1 < array.length) {
-//     let index2 = 0
-//     while (index2 < array.length) {
-//       if (result.length === 0) {
-//         if (index1 !== index2) {
-//           if (array[index1] + array[index2] === targetSum) {
-//             result.push(array[index1])
-//             result.push(array[index2])
-//           }
-//         }
-//       }
-//       index2 += 1
-//     }
-//     index1 += 1
-//   }
-//   return result
-// }
+  while (low <= high) {
+    let middle = Math.floor((low + high) / 2)
 
-// output = twoNumberSum([3, 5, -14, 8, 11, 1, -1, 6], 10)
-// console.log(output)
-
-// Bubble Sort
-
-// this doesnt work I need to learn for loops
-// const bubbleSort = function (array) {
-//   let sorted = false
-//   let index = 0
-//   while (!sorted) {
-//     sorted = true
-//   }
-//   while (index < bubble.length - 1) {
-//     if (bubble[index] > bubble[index + 1]) {
-//       x = bubble[index]
-//       bubble[index] = bubble[index + 1]
-//       bubble[index + 1] = x
-//     }
-//     index += 1
-//   }
-//   return array
-// }
-// console.log(
-//   bubbleSort([
-//     1,
-//     0,
-//     -10,
-//     10000,
-//     993423,
-//     234238423402384,
-//     11,
-//     11,
-//     12,
-//     12,
-//     1239012309128312,
-//     1223,
-//     45065,
-//     55,
-//     0344342,
-//     0234,
-//     111,
-//   ])
-// )
-
-// A Very Big Sum
-
-// let numbers = [1000000001, 1000000002, 1000000003, 1000000004, 1000000005]
-
-// function aVeryBigSum(ar) {
-
-//   let result = 0
-//   ar.forEach(function (number) {
-//     result = result + number
-//   })
-//   return result
-
-// }
-
-// console.log(aVeryBigSum(numbers))
-
-// isPalindrome
-
-// function isPalindrome(string) {
-//   let reversedString = ""
-//   for (let i = string.length - 1; i >= 0; i--) {
-//     reversedString += string[i]
-//   }
-//   return string === reversedString
-// }
-// console.log(isPalindrome("racecar"))
-
-// // create function
-// function isPalindrome(string) {
-//   // create array to store each letter of the string
-//   let reversedString = []
-//   // create for loop. index is equal to string length -1, runs until 1 is 0, and decreases 1 each time.
-//   for (let i = string.length - 1; i >= 0; i--) {
-//     // add each element of the string, strarting at the end, to the array
-//     reversedString.push(string[i])
-//   }
-//   // join's the reversed string together and checks if it is equal to string. Will return true if is or false if not
-//   return string === reversedString.join("")
-// }
-// // run function
-// console.log(isPalindrome("racecar"))
-
-
-/* SOLUTION 1 Pseudocode out your solution */
-
-
-
-// /* SOLUTION 2 Start a time and track how long it takes you. This solution is meant to be the ugly solution. The one that you get working the fastest at any cost */
-// function reduceDirections2(directions) {
-//   // TODO: ADD LOGIC HERE
-//   return ['']
-// }
-
-// /* SOLUTION 3 Review your previous solution and refactor it. Do you best to not reach for outside help. I want the best solution you know how to write with your current knowledge */
-// function reduceDirections3(directions) {
-//   // TODO: ADD LOGIC HERE
-//   return ['']
-// }
-
-
-
-
-// const minus = function (numberArray) {
-//   return numberArray.splice(1)
-// }
-
-// console.log(minus([15, 16, 17]))
-
-const golomb = function (n, memo = {}) {
-  if (n === 1) {
-    return 1
+    if (inputArray[middle] === target) {
+      return middle
+    } else if (inputArray[middle] < target) {
+      low = middle + 1
+    } else {
+      high = middle - 1
+    }
   }
-  if (!memo[n]) {
-    memo[n] = 1 + golomb(n - golomb(golomb(n - 1, memo), memo), memo)
-  }
-  return memo[n]
+  return - 1
 }
 
-console.log(golomb(100))
+let findPeak = (inputArray) => {
+  let left = 0
+  let right = inputArray.length - 1
+
+  while (left < right) {
+    let middle = Math.floor((low + high) / 2)
+    if (inputArray[low] < inputArray[low + 1]) {
+      left = middle + 1
+    } else {
+      right = middle
+    }
+  }
+  return left
+}
