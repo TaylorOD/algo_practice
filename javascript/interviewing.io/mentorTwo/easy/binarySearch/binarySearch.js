56518f04267fe62a16de8643afce63d117e3af12
taylorod <taylorsunwantedemails@gmail.com>
2021-12-10 12:30:44 -0500

    add binary search again

--- javascript/interviewing.io/mentorTwo/easy/binarySearch/binarySearch.js
+++ javascript/interviewing.io/mentorTwo/easy/binarySearch/binarySearch.js
@@ -36,16 +36,16 @@ console.log(binarySearch([-1, 0, 3, 5, 9, 12], 9))
 console.log(binarySearch([-1, 0, 3, 5, 9, 12], 2))
 
 const binarySearchLeet = (inputArray, target) => {
-  let low = 0
-  let high = inputArray.length - 1
-  while (low <= high) {
-    let middle = Math.floor((low + high) / 2)
+  let left = 0
+  let right = inputArray.length - 1
+  while (left <= right) {
+    let middle = Math.floor((left + right) / 2)
     if (inputArray[middle] === target) {
       return middle
-    } else if (inputArray[middle] < target) {
-      low = middle + 1
+    } else if(inputArray[middle] < target) {
+      left = middle + 1
     } else {
-      high = middle -1
+      right = middle - 1
     }
   }
   return -1
